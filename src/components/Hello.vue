4b8724b5295f25c282958f7fff7f98d87fa827e1
Alex Mendes <alex.mendes@bl.uk>
2017-05-25 16:19:31 +0100

    Add Bootstrap Vue

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -31,8 +31,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h1, h2 {
   font-weight: normal;
 }
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import BootstrapVue from 'bootstrap-vue'
 import router from './router'
+
+Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
 
