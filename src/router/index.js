faffb8e35bbe03f0ac2d4d89d054692cf5afa508
Alex Mendes <alex.mendes@bl.uk>
2017-05-30 12:44:25 +0100

    Add playbills route

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Landing from '@/pages/Landing'
+import Error from '@/pages/Error'
 
 Vue.use(Router)
 
@@ -10,6 +11,16 @@ export default new Router({
       path: '/',
       name: 'landing',
       component: Landing
+    },
+    {
+      path: '/site/playbills',
+      name: 'playbills',
+      component: Landing,
+      props: {
+        pybossaApi: process.env.NODE_ENV === 'production'
+                      ? 'http://'
+                      : 'http://localhost:5000'
+      }
     }
   ]
 })
