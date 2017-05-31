125894bdcd00eba8fe5c0908fa5ae639ee132bed
Alex Mendes <alex.mendes@bl.uk>
2017-05-31 13:35:52 +0100

    Update site config

--- src/config.js
+++ src/config.js
@@ -2,13 +2,21 @@ export default {
   sites: {
     dev: {
       pbHost: 'http://127.0.0.1:5000',
+      title: 'Some project',
       bgImg: require('@/assets/img/code.png'),
-      about: '# About the project \n Blurb'
+      about: '# About the project \n Blurb',
+      team: [
+
+      ]
     },
     playbills: {
       pbHost: 'http://138.68.135.24:5000',
+      title: 'Playbills',
       bgImg: require('@/assets/img/playbills.jpg'),
-      about: require('@/assets/markdown/playbills.md')
+      about: require('@/assets/markdown/playbills.md'),
+      team: [
+
+      ]
     }
   }
 }
