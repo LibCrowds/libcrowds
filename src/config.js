9a0ca990647cc92d7c0775edc947c6237402ee62
Alexander Mendes <alexanderhmendes@gmail.com>
2017-06-04 21:23:02 +0100

    Update config

--- src/config.js
+++ src/config.js
@@ -3,9 +3,10 @@ const isDev = process.env.NODE_ENV === 'development'
 export default {
   brand: 'LibCrowds',
   contact: {
-    twitter: '@LibCrowds',
+    twitter: 'LibCrowds',
     email: 'crowdsourcing@bl.uk'
   },
+  discourseUrl: 'http://community.libcrowds.com',
   pybossaSites: {
     playbills: {
       host: isDev ? 'http://localhost:5000' : 'http://138.68.135.24:5000',
