ecaea48a8e5c044def6d6ecb363ba8281c202634
Alex Mendes <alexanderhmendes@gmail.com>
2017-06-07 01:14:33 +0100

    Keep to 80 chars

--- src/pages/pybossa/help/TOS.vue
+++ src/pages/pybossa/help/TOS.vue
@@ -1,167 +1,212 @@
 <template>
   <section>
     <p>
-      These are the general terms and conditions on which we supply all our services. If you use our services,
-      you agree to abide by these terms.
+      These are the general terms and conditions on which we supply all our
+      services. If you use our services, you agree to abide by these terms.
     </p>
     <p>
-      We may update these terms and conditions at any time. If we do so, we will announce the change on our blog.
-      Any changes will be binding on you from the moment we announce them.
+      We may update these terms and conditions at any time. If we do so, we will
+      announce the change on our blog. Any changes will be binding on you from
+      the moment we announce them.
     </p>
     <p>
       This agreement is made under the laws of England and Wales.
     </p>
     <p>
-      At {{ config.brand }} we supply a lot of different services. Some of them will have specific terms tailored for 
-      them. If that is the case, {{ config.brand }}’s contract with you for that service will be on these terms, 
-      supplemented by any terms specific to the service. In the case of any conflict, service specific terms will take 
-      precedence.
+      At {{ config.brand }} we supply a lot of different services. Some of them
+      will have specific terms tailored for them. If that is the case,
+      {{ config.brand }}’s contract with you for that service will be on these
+      terms, supplemented by any terms specific to the service. In the case of
+      any conflict, service specific terms will take precedence.
     </p>
     <p>
-      We process personal data in accordance with our Privacy Policy. As part of this agreement you consent to our 
-      doing so. You should read the policy carefully, especially if you have any concerns about your privacy.
+      We process personal data in accordance with our Privacy Policy. As part of
+      this agreement you consent to our doing so. You should read the policy
+      carefully, especially if you have any concerns about your privacy.
     </p>
     <p>
-      Unless we have agreed a particular level of service with you, we make absolutely no promises about the
-      quality or existence of any of our services. Please read the sections below and our general exclusion of 
+      Unless we have agreed a particular level of service with you, we make
+      absolutely no promises about the quality or existence of any of our
+      services. Please read the sections below and our general exclusion of
       liability.
     </p>
     <p>
-      All use of our web services are subject to our terms for web users. There are further terms if you have signed up
-      for an account with one of our services.
+      All use of our web services are subject to our terms for web users. There
+      are further terms if you have signed up for an account with one of our
+      services.
     </p>
     <h3>Terms for use of our web services</h3>
     <p>
       Web services include our API’s as well as any of our websites.
     </p>
     <p>
-      While we are proud of the services we provide to the world for free and try to make them reliable and useful,
-      we make no promises about them. All web services are certain to fail some of the time. We adapt and change
-      our services from time to time, so you may find that something that previously worked for you may cease to work.
-      We may also stop supplying any service, temporarily or permanently or block access to our services to anyone for
-      any reason.
+      While we are proud of the services we provide to the world for free and
+      try to make them reliable and useful, we make no promises about them. All
+      web services are certain to fail some of the time. We adapt and change
+      our services from time to time, so you may find that something that
+      previously worked for you may cease to work. We may also stop supplying
+      any service, temporarily or permanently or block access to our services to
+      anyone for any reason.
     </p>
     <p>
-      We are not a law firm and nothing we do is intended to be taken as legal advice. In particular, while we encourage
-      the use of one of the <a href="http://okfn.org/licenses/">open data commons licences from the Open Knowledge 
-      Foundation</a> we cannot give any warranty that they will work in the way expected or should be used for any 
-      specific purpose. If in doubt, you should take your own legal advice.
+      We are not a law firm and nothing we do is intended to be taken as legal
+      advice. In particular, while we encourage the use of one of the open data
+      commons licences from the
+      <a href="http://okfn.org/" target="_blank">Open Knowledge Foundation</a>
+      we cannot give any warranty that they will work in the way expected or
+      should be used for any specific purpose. If in doubt, you should take
+      your own legal advice.
     </p>
     <h3>What you agree</h3>
     <p>
       You agree not to use our websites to do any of the following:
     </p>
     <ul>
-      <li>anything which is illegal either where you are in the world, or where we are</li>
+      <li>
+        anything which is illegal either where you are in the world,
+        or where we are
+      </li>
       <li>cause nuisance to other users of our services</li>
       <li>interfere with the normal running of our services</li>
       <li>
-        try to access our systems in a way other than those advertised by us and, in particular, to use a web crawler
-        that does not respect the robots exclusion policy.
+        try to access our systems in a way other than those advertised by us
+        and, in particular, to use a web crawler that does not respect the
+        robots exclusion policy.
       </li>
     </ul>
     <h3>Other websites</h3>
     <p>
-      Some of our activities are carried out on web platforms provided by third parties. For example the source code for 
-      our projects and plugins are hosted on Github. If you make use of any service where that is the case, you are 
-      responsible for complying with any terms of service of the third party platform.
+      Some of our activities are carried out on web platforms provided by third
+      parties. For example the source code for our projects and plugins are
+      hosted on Github. If you make use of any service where that is the case,
+      you are responsible for complying with any terms of service of the third
+      party platform.
     </p>
     <h3>Accounts</h3>
     <p>
-      Some of our services require you to create an account in order to make certain, or any, use of the service.
-      All our accounts are subject to the following rules:
+      Some of our services require you to create an account in order to make
+      certain, or any, use of the service. All our accounts are subject to the
+      following rules:
     </p>
     <ul>
       <li>You must be at least 13 years old and a human being.</li>
       <li>You must supply us with a valid e-mail address.</li>
       <li>
-        You are responsible for the security of your accounts and making sure that any contact
-        details in the account are kept up to date. If we need to contact you but are unable to do so,
-        for example because your e-mail address is no longer valid, then any consequences of that failure
+        You are responsible for the security of your accounts and making sure
+        that any contact details in the account are kept up to date. If we need
+        to contact you but are unable to do so, for example because your e-mail
+        address is no longer valid, then any consequences of that failure
         will be your responsibility.
       </li>
       <li>
-        You must not let anyone else use your account. If pressure is applied to you to do so — for example if an 
-        employer demands your username and password — please inform them that their attempt to subvert your agreement 
-        with us will mean that they have no permission to use any of our services. We may take action, including 
-        criminal prosecution, if they use our services using an account they have obtained in this way.
+        You must not let anyone else use your account. If pressure is applied to
+        you to do so — for example if an employer demands your username and
+        password — please inform them that their attempt to subvert your
+        agreement with us will mean that they have no permission to use any of
+        our services. We may take action, including criminal prosecution, if
+        they use our services using an account they have obtained in this way.
       </li>
       <li>
-        You must let us know of any unauthorised use of your account as soon as you are able to after becoming aware of 
-        it.
+        You must let us know of any unauthorised use of your account as soon as
+        you are able to after becoming aware of it.
       </li>
-      <li>We may suspend or terminate your account at any time. Equally, you may close your account at any time</li>
+      <li>
+        We may suspend or terminate your account at any time. Equally, you
+        may close your account at any time.
+      </li>
     </ul>
     <h3>Membership</h3>
     <p>
-      Our most important class of account is one you may create in order to become a member. By registering as a 
-      member, you are acknowledging your connection to us. You are subject to any rules for community members we may 
-      publish and we may send you email messages we think appropriate for members, for example in order to poll you on 
-      some important issue.
+      Our most important class of account is one you may create in order to
+      become a {{ config.brand }} member. By registering as a member, you are
+      acknowledging your connection to us. You are subject to any rules for
+      community members we may publish and we may send you email messages we
+      think appropriate for members, for example in order to poll you on some
+      important issue.
     </p>
     <p>
-      Membership is not membership in the formal sense of membership of a company limited by guarantee.
+      Membership is not membership in the formal sense of membership of a
+      company limited by guarantee.
     </p>
     <h3>Content and intellectual property</h3>
     <p>
-      If you contribute content to any of our services, for example by commenting on a task, or uploading data, then as 
-      a general rule you agree to licence that content to us under the same licence as prevails for that service or 
-      website.
+      If you contribute content to any of our services, for example by
+      commenting on a task, or uploading data, then as a general rule you agree
+      to licence that content to us under the same licence as prevails for that
+      service or website.
     </p>
     <p>
-      Unless otherwise stated all our services are offered under open content or data licences and you should refer to 
-      the provisions of the licence in question to find out what you are allowed to do. Some of our content belongs to 
-      third parties. Most third party data is subject to an open licence, but we cannot guarantee it. You should 
-      refer to the third party if you are in doubt.
+      Unless otherwise stated all our services are offered under open content or
+      data licences and you should refer to the provisions of the licence in
+      question to find out what you are allowed to do. Some of our content
+      belongs to third parties. Most third party data is subject to an open
+      licence, but we cannot guarantee it. You should refer to the third party
+      if you are in doubt.
     </p>
     <p>
-      As a general rule, this agreement will not change the ownership of any intellectual property belonging to either 
-      party. Where your content is used by us or vice versa both you and we would do so under a licence (see above).
+      As a general rule, this agreement will not change the ownership of any
+      intellectual property belonging to either party. Where your content is
+      used by us or vice versa both you and we would do so under a licence
+      (see above).
     </p>
     <h3>Liability</h3>
     <p>
-      If you breach any of your obligations under this agreement and, as a result, cause us to be sued by anyone else, 
-      you will have to compensate us for any loss we have suffered as a result, which includes any costs, such as paying 
-      lawyers, or for our own time, we incur defending a claim as well as any damages awarded. If your breach causes you 
-      to be sued by someone else, you will not sue us for any loss you suffer as a result.
+      If you breach any of your obligations under this agreement and, as a
+      result, cause us to be sued by anyone else, you will have to compensate us
+      for any loss we have suffered as a result, which includes any costs, such
+      as paying lawyers, or for our own time, we incur defending a claim as well
+      as any damages awarded. If your breach causes you to be sued by someone
+      else, you will not sue us for any loss you suffer as a result.
     </p>
     <p>
-      We limit our liability in several different ways — all of which we believe to be fair. In case any one of them is 
-      found to be unenforceable by a court, each of the following limitations of liability is separate and our liability 
-      to you is limited by all of them:
+      We limit our liability in several different ways — all of which we believe
+      to be fair. In case any one of them is found to be unenforceable by a
+      court, each of the following limitations of liability is separate and our
+      liability to you is limited by all of them:
     </p>
     <ul>
       <li>
-        All exclusions of liability are only in so far as we are allowed to do so by whatever law applies to the 
-        situation. For example, the law of England and Wales prevents us from excluding our liability for any personal 
-        injury or death caused by our negligence. We do not exclude such liability.
+        All exclusions of liability are only in so far as we are allowed to do
+        so by whatever law applies to the situation. For example, the law of
+        England and Wales prevents us from excluding our liability for any
+        personal injury or death caused by our negligence. We do not exclude
+        such liability.
       </li>
       <li>
-        We will not be liable for any damage that was not reasonably foreseeable at the time we made this agreement.
+        We will not be liable for any damage that was not reasonably foreseeable
+        at the time we made this agreement.
       </li>
       <li>
-        We are not liable for any loss which is indirect or consequential. That includes any loss of business or profit.
+        We are not liable for any loss which is indirect or consequential. That
+        includes any loss of business or profit.
       </li>
-      <li>We exclude, in so far as we are allowed, any warranties that would be implied by law.</li>
+      <li>
+        We exclude, in so far as we are allowed, any warranties that would be
+        implied by law.
+      </li>
     </ul>
     <h3>Boilerplate</h3>
     <p>
-      These final “boilerplate” terms of should go without saying, but we are saying them anyway just to be clear.
+      These final “boilerplate” terms of should go without saying, but we are
+      saying them anyway just to be clear.
     </p>
     <p>
-      If any part of this agreement is ineffective (for example because it is unlawful) then the rest of the agreement 
-      should be read without it.
+      If any part of this agreement is ineffective (for example because it is
+      unlawful) then the rest of the agreement should be read without it.
     </p>
     <p>
-      This agreement is between you and us and is not intended to give anyone else any rights.
+      This agreement is between you and us and is not intended to give anyone
+      else any rights.
     </p>
     <p>
-      We may sometimes fail to enforce our rights under this agreement (for example because we decide not to, or we did 
-      not realise you were in breach of contract). Just because we have not enforced any of our rights, does not stop us 
-      from doing so in the future.
+      We may sometimes fail to enforce our rights under this agreement (for
+      example because we decide not to, or we did not realise you were in breach
+      of contract). Just because we have not enforced any of our rights, does
+      not stop us from doing so in the future.
     </p>
     <p>
-      Neither party is liable for anything which is beyond their reasonable control.
+      Neither party is liable for anything which is beyond their reasonable
+      control.
     </p>
   </section>
 </template>
