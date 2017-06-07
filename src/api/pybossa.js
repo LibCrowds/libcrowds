bec59e1e49e0cf1f9926ee017409dca8ad3b2248
Alex Mendes <alex.mendes@bl.uk>
2017-06-07 12:45:53 +0100

    Handle 'next' responses

--- src/api/pybossa.js
+++ src/api/pybossa.js
@@ -4,11 +4,24 @@ import axios from 'axios'
  * Return a configured axios instance.
  */
 export const create = (siteConfig) => {
-  return axios.create({
+  let instance = axios.create({
     baseURL: siteConfig.host,
     headers: {
       'Content-Type': 'application/json'
     },
     data: {}  // Must always be set otherwise Content-Type gets deleted
   })
+
+  instance.interceptors.response.use((res) => {
+    if ('next' in res.data) {
+      return instance.get(res.data.next)
+    }
+    return res
+  }, function (error) {
+    
+    // TODO: handle response errors
+    return Promise.reject(error)
+  })
+
+  return instance
 }
