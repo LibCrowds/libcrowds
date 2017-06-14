3b8371bfb631db213866bf2078e66af7f6e8ac99
Alex Mendes <alexanderhmendes@gmail.com>
2017-06-14 15:32:44 +0100

    Add site home

---
+++ src/pages/pybossa/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+
+  <site-base>
+
+    <section>
+      <h1>Playbills</h1>
+    </section>
+
+  </site-base>
+
+</template>
+
+<script>
+import SiteBase from '@/components/layouts/SiteBase'
+
+export default {
+  data: function () {
+    return {
+      siteConfig: this.$store.state.siteConfig
+    }
+  },
+
+  components: {
+    SiteBase
+  }
+}
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,9 @@
 import config from '@/config'
 import store from '@/store'
 
-import Core from '@/components/layouts/Core'
 import Landing from '@/pages/Landing'
 
+import Home from '@/pages/pybossa/home/Home'
 import About from '@/pages/pybossa/home/About'
 import Data from '@/pages/pybossa/home/Data'
 import TOS from '@/pages/pybossa/help/TOS'
@@ -16,7 +16,7 @@ import Projects from '@/pages/pybossa/Projects/Projects'
 export default [
 
   // Core site
-  { path: '/', name: 'core', component: Core },
+  { path: '/', name: 'core', component: Landing },
   { path: '/help/tos', name: 'tos', component: TOS },
   { path: '/help/privacy', name: 'privacy', component: Privacy },
   { path: '/help/cookies', name: 'cookies', component: Cookies },
@@ -44,7 +44,7 @@ export default [
       next()
     },
     children: [
-      { path: '', name: 'home', component: Landing },
+      { path: '', name: 'home', component: Home },
       { path: 'about', name: 'about', component: About },
       { path: 'data', name: 'data', component: Data },
       { path: 'projects', name: 'projects', component: Projects }
