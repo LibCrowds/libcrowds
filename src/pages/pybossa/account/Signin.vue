a9a1c7587291d8d784e8f8bc1a91314df5123f54
Alex Mendes <alexanderhmendes@gmail.com>
2017-06-14 21:27:35 +0100

    Move sign in form to single component

--- src/pages/pybossa/account/Signin.vue
+++ src/pages/pybossa/account/Signin.vue
@@ -1,23 +1,85 @@
 <template>
-  <signin-form></signin-form>
+  <b-card class="mb-2">
+
+    <b-form-fieldset
+      label="Email">
+      <b-form-input
+        type="text"
+        label="email"
+        :feedback="form.errors.email"
+        :state="getState('email')"
+        placeholder="Enter your email address"
+        v-model="form.email">
+      </b-form-input>
+    </b-form-fieldset>
+
+    <b-form-fieldset
+      label="Password">
+      <b-form-input
+        type="text"
+        label="Password"
+        :feedback="form.errors.password"
+        :state="'password' in form.errors ? 'error' : null"
+        placeholder="Enter your password"
+        v-model="form.password">
+      </b-form-input>
+    </b-form-fieldset>
+
+    <b-button variant="outline-secondary">Cancel</b-button>
+    <b-button variant="success" v-on:click="submit">Sign</b-button>
+
+  </b-card>
 </template>
 
 <script>
-import SigninForm from '@/components/forms/Signin'
-import config from '@/config'
+import pybossaApi from '@/api/pybossa'
 
 export default {
   data: function () {
     return {
-      config: config
+      form: {
+        csrf: null,
+        email: null,
+        errors: {},
+        password: null
+      }
     }
   },
 
-  components: {
-    SigninForm
+  computed: {
+
+  },
+
+  methods: {
+    fetchData () {
+      pybossaApi.get('/account/signin').then(res => {
+        this.form = res.data.form
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    submit () {
+      pybossaApi.post('/account/signin', this.form, {
+        headers: {
+          'X-CSRFToken': this.form.csrf
+        }
+      }).then(res => {
+        this.form = res.data.form
+      }).catch(error => {
+        console.log(error.response)
+      })
+    },
+    getFeedback (field) {
+      return this.form.errors[field]
+    },
+    getState (field) {
+      console.log(this.form.errors.email)
+      return field in this.form.errors ? 'error' : null
+    }
+  },
+
+  mounted () {
+    this.fetchData()
   }
 }
 </script>
-
-<style>
-</style>
