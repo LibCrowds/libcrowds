be52b2c184b8a12a52e6c2daad4b8b66535df9dd
Alex Mendes <alex.mendes@bl.uk>
2017-06-15 15:42:34 +0100

    Add sections to landing page

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -13,6 +13,59 @@
         </div>
       </a>
     </crowd-container>
+    
+    <section id="data">
+      <div class="jumbotron">
+        <div class="container text-center">
+          <div class="row py-2">
+            <div class="col-sm-12">
+              <h2 class="display-3">Open Data</h2>
+            </div>
+          </div>
+          <div class="row py-3">
+            <div class="col-md-6 offset-md-3">
+              <p class="lead">
+                All datasets generated from the experimental crowdsourcing 
+                projects hosted on this platform are made available under a
+                <a :href="config.dataLicense.url" target="_blank">
+                  {{ config.dataLicense.name }}
+                </a> license. Datasets can be downloaded in JSON or CSV 
+                formats. Visit the data page for each project website to find 
+                out more.
+              </p>
+            </div>
+          </div>
+          <b-button
+            variant="outline-white">
+            Choose a project
+          </b-button>
+        </div>
+      </div>
+    </section>
+    
+    <section id="community" class="bg-faded" v-if="config.discourseUrl.length">
+      <div class="container py-5 text-center">
+        <div class="row">
+          <div class="col-sm-12">
+            <h3 class="text-uppercase font-weight-bold display-4">
+              {{ config.brand }} Community
+            </h3>
+            <hr class="bw-3">
+            <p class="text-left">
+              Discovered something interesting? Got a suggestion for a cool
+              new project? Developing something using our crowdsourced data?
+              Our community forum is the place to discuss your thoughts
+              and ideas with {{ config.brand }} volunteers, researchers and
+              developers.
+            </p>
+          </div>
+        </div>
+        <b-button
+          variant="secondary">
+          Choose a project
+        </b-button>
+      </div>
+    </section>
 
     <site-footer></site-footer>
   </div>
@@ -20,12 +73,14 @@
 
 <script>
 import 'vue-awesome/icons/angle-down'
+import config from '@/config'
 import CrowdContainer from '@/components/layouts/CrowdContainer'
 import SiteFooter from '@/components/SiteFooter'
 
 export default {
   data: function () {
     return {
+      config: config,
       jtStyle: {
         filter: 'brightness(100%)'
       },
@@ -51,11 +106,23 @@ h1 {
   margin-bottom: 0;
 }
 
+.btn-outline-white {
+  @include button-outline-variant($white, $black);
+}
+
 #scrollDown p {
   font-size: $font-size-sm;
   color: $white;
   text-transform: uppercase;
   margin-bottom: 0.2em;
+}
+
+#data {
+  .jumbotron {
+    margin-bottom: 0;
+    color: $white;
+    background: url('../assets/img/code.png') no-repeat left fixed;
+  }
 }
 
 .pulse {
