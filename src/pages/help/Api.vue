7da7d50ec564a7001bfc49d1121d292eff78fa0d
Alex Mendes <alexanderhmendes@gmail.com>
2017-06-15 02:59:28 +0100

    Reorganise pages

---
+++ src/pages/help/Api.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+    <p>
+      {{ config.brand }} provides a RESTful API that can be used for retreiving
+      information about tasks, task runs, results, etc. The API expects and 
+      returns JSON and is available at:
+    </p>
+    <pre class="my-2">
+      <code>
+        {{ config.pybossaHost }}/api
+      </code>
+    </pre>
+    <p>
+      Some requests will need an API key to authenticate and authorize the 
+      operation. You can find your API key listed in your profile.
+    </p>
+    <p>
+      Rather than replicating the documentation here please refer to the 
+      official
+      <a href="http://docs.pybossa.com/en/latest/api.html">
+        PYBOSSA API documentation
+      </a> 
+      for details.
+    </p>
+  </section>
+</template>
+
+<script>
+import config from '@/config'
+
+export default {
+  data: function () {
+    return {
+      config: config
+    }
+  }
+}
+</script>
+
+<style>
+h3 {
+  margin-top: 3rem;
+}
+</style>
