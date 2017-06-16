0d28dabec73938af0058e985fa557acd663064c0
Alex Mendes <alex.mendes@bl.uk>
2017-06-16 16:42:53 +0100

    Add data, featured and top users to landing page

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -52,8 +52,111 @@
             <p>Volunteers</p>
           </div>
         </div>
-    </div>
-</section>
+      </div>
+    </section>
+    
+    <section id="featured-projects" class="invert-navbar" v-if="featured">
+      <div class="container text-center text-lg-left">
+        <div class="row">
+          <div class="col-lg-5 offset-lg-1 text-uppercase py-5">
+            <h2 class="font-weight-bold display-4">
+              Featured Projects
+            </h2>
+            <hr class="wide mr-lg-5">
+            <p class="mr-lg-6">
+              Choose from some of our current favourites.
+            </p>
+          </div>
+          <div class="col-lg-3 offset-lg-3 hidden-md-down">
+            <span id="ribbon">
+              <icon name="star" scale="7"></icon>
+            </span>
+          </div>
+        </div>
+        <div class="row my-5">
+          <div class="col-lg-10 offset-lg-1">
+            Load projects here
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section id="data">
+      <b-jumbotron>
+        <div class="container py-4">
+          <div class="row py-2 text-center">
+            <div class="col-sm-12">
+              <h3 class="display-2">Open Data</h3>
+            </div>
+          </div>
+          <div class="row py-3">
+            <div class="col-sm-6 offset-sm-3">
+              <p class="lead">
+                All datasets generated from the crowdsourcing projects hosted 
+                on this platform are made available under a
+                <a :href="config.dataLicense.url" target="_blank">
+                  {{ config.dataLicense.name }}
+                </a> license. Datasets can be downloaded by anyone in JSON and 
+                CSV formats, visit our data page to find out more.
+              </p>
+            </div>
+          </div>
+          <div class="row py-3 text-center">
+            <div class="col-sm-12">
+              <b-button variant="outline-white" :to="{ name: 'data' }">
+                Get the data
+              </b-button>
+            </div>
+          </div>
+        </div>
+      </b-jumbotron>
+    </section>
+    
+    <section 
+      id="top-users" 
+      class="bg-white invert-navbar" 
+      v-if="topUsers.length">
+      <div class="container py-5">
+        <div class="row">
+          <div class="col-lg-5 offset-lg-1 pt-3 text-center">
+            <h3 class="text-uppercase font-weight-bold display-4">
+              Most Active Volunteers
+            </h3>
+            <hr class="wide w-50">
+            <p class="text-uppercase lead">
+              To date, our top {{ topUsers.length }} volunteers have made an 
+              amazing N contributions!
+            </p>
+            <b-button variant="secondary" :to="{ name: 'projects' }">
+              Get Involved
+            </b-button>
+          </div>
+          <div class="col-lg-5 offset-lg-1 hidden-md-down">
+            <img src="../assets/img/wreath.png" alt="Wreath" class="img-fluid">
+            <span id="wreath"></span>
+          </div>
+        </div>
+        <div class="row text-center mt-4">
+          <div class="col-lg-12 pt-2">
+            Render Avatar list
+          </div>
+        </div>
+        <div class="row text-center mt-1">
+          <div class="col-sm-12 col-lg-5 offset-lg-1 push-lg-6 mt-4">
+            <a href="#" class="black-ul-link">
+              <icon name="eye"></icon> View the leaderboard
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    <section id="social-media" class="invert-navbar bg-white">
+      <div class="container py-5">
+        <hr>
+        Social media buttons helper
+      </div>
+    </section>
 
   </div>
 
@@ -64,6 +167,8 @@ import ScrollReveal from 'scrollreveal'
 import 'vue-awesome/icons/desktop'
 import 'vue-awesome/icons/tasks'
 import 'vue-awesome/icons/users'
+import 'vue-awesome/icons/star'
+import 'vue-awesome/icons/eye'
 import config from '@/config'
 import pybossaApi from '@/api/pybossa'
 
@@ -72,7 +177,9 @@ export default {
   data: function () {
     return {
       config: config,
-      stats: {}
+      stats: {},
+      featured: [],
+      topUsers: []
     }
   },
 
@@ -84,19 +191,25 @@ export default {
   },
 
   methods: {
-    fetchStats () {
+    fetchData () {
       pybossaApi.get('stats/').then(r => {
         this.stats = r.data.stats
+      })
+      pybossaApi.get('/').then(r => {
+        if ('featured' in r.categoriesProjects) {
+          this.featured = r.categoriesProjects.featured
+        }
+        this.topUsers = r.data.top_users
       })
     },
     scrollReveal () {
       const sr = ScrollReveal()
-      sr.reveal('.sr', { delay: 400, duration: 600 }, 75)
+      sr.reveal('.sr', { delay: 300, duration: 600 }, 75)
     }
   },
 
   mounted () {
-    this.fetchStats()
+    this.fetchData()
     this.scrollReveal()
   }
 }
@@ -129,6 +242,72 @@ export default {
   
   .font-family-serif {
     font-family: $font-family-serif;
+  }
+  
+  hr.wide {
+    border-width: 3px;
+  }
+  
+  .btn-outline-white {
+    @include button-outline-variant($white, $black);
+  }
+  
+  .black-ul-link  {
+    color: $black;
+    border-bottom: 3px solid rgba($black, 0);
+    display: inline-block;
+    transition: border-bottom 500ms;
+
+    @include hover-focus {
+      border-bottom: 3px solid rgba($black, 1);
+    }
+  }
+  
+  #featured-projects {
+    color: $gray-dark;
+    background-image: url('../assets/img/white-wall.png');
+    box-shadow: 0 0 3px rgba($black, 0.2);
+    -webkit-box-shadow: 0 0 3px rgba($black, 0.2);
+    border: 1px solid $white;
+  }
+  
+  #ribbon {
+    text-align: center;
+    display:block;
+    width: 12rem;
+    height: 12rem;
+    background: $gray-lighter;
+    position: absolute;
+    top: -1px;
+
+    svg {
+      margin-top: 4rem;
+      color: darken($body-bg, 3%);
+    }
+
+    &:after {
+      content: "";
+      display: block;
+      position: relative;
+      width: 0;
+      height: 0;
+      border-width: 2rem 6rem 4rem 6rem;
+      border-style: solid;
+      border-color: $gray-lighter $gray-lighter transparent $gray-lighter;
+    }
+  }
+  
+  #data {
+    .jumbotron {
+      color: $white;
+      font-weight: 300;
+      background: url('../assets/img/code.png') no-repeat left fixed;
+      margin-bottom: 0;
+    }
+  }
+  
+  #top-users {
+    color: $gray-dark;
   }
 }
 </style>
