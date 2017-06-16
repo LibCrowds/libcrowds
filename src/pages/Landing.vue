765218dbae7f1be5f0c793b26f2b441e395d062e
Alex Mendes <alex.mendes@bl.uk>
2017-06-16 14:38:49 +0100

    Add intro section to about page

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,9 +1,9 @@
 <template>
 
-  <default-layout>
+  <div id="landing">
 
     <transition appear>
-      <div class="container text-center">
+      <div class="container full-height">
         <h1 id="tagline">{{ config.tagline }}</h1>
         <div id="intro-buttons" class="my-3">
           <b-button variant="secondary" :to="{ name: 'about' }">
@@ -15,20 +15,64 @@
         </div>
       </div>
     </transition>
+    
+    <section id="intro" class="bg-white invert-navbar">
+      <div class="container py-5 text-center">
+        <div class="row sr">
+          <div class="col-sm-10 offset-sm-1">
+            <p class="font-family-serif display-4 mb-0">
+              Have a direct impact on enabling future research at  
+              {{ config.company }}.
+            </p>
+          </div>
+        </div>
+        <hr class="my-4 w-75 sr">
+        <div class="row sr">
+          <div class="col-sm-10 offset-sm-1">
+            <p class="lead my-2">
+              {{ config.description }}
+            </p>
+          </div>
+        </div>
+        <hr class="my-4 w-75 sr">
+        <div class="row">
+          <div class="col-md-4 my-3 sr">
+            <icon name="desktop" scale="4"></icon>
+            <p class="my-1">{{ stats.n_published_projects }}</p>
+            <p>Projects</p>
+          </div>
+          <div class="col-md-4 my-3 sr">
+            <icon name="tasks" scale="4"></icon>
+            <p class="my-1">{{ stats.n_task_runs }}</p>
+            <p>Contributions</p>
+          </div>
+          <div class="col-md-4 my-3 sr">
+            <icon name="users" scale="4"></icon>
+            <p class="my-1">{{ stats.n_total_users }}</p>
+            <p>Volunteers</p>
+          </div>
+        </div>
+    </div>
+</section>
 
-  </default-layout>
+  </div>
 
 </template>
 
 <script>
+import ScrollReveal from 'scrollreveal'
+import 'vue-awesome/icons/desktop'
+import 'vue-awesome/icons/tasks'
+import 'vue-awesome/icons/users'
 import config from '@/config'
-import DefaultLayout from '@/components/layouts/Default'
+import pybossaApi from '@/api/pybossa'
 
 export default {
 
   data: function () {
     return {
-      config: config
+      config: config,
+      stats: {}
     }
   },
 
@@ -39,8 +83,21 @@ export default {
     ]
   },
 
-  components: {
-    DefaultLayout
+  methods: {
+    fetchStats () {
+      pybossaApi.get('stats/').then(r => {
+        this.stats = r.data.stats
+      })
+    },
+    scrollReveal () {
+      const sr = ScrollReveal()
+      sr.reveal('.sr', { delay: 400, duration: 600 }, 75)
+    }
+  },
+
+  mounted () {
+    this.fetchStats()
+    this.scrollReveal()
   }
 }
 </script>
@@ -49,14 +106,29 @@ export default {
 @import 'src/assets/style/_vars';
 @import '~bootstrap/scss/bootstrap';
 
-.container {
-  opacity: 1;
-  transition: opacity 800ms;
+#landing {
+  .container.full-height {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    color: $white;
+    opacity: 1;
+    transition: opacity 800ms;
 
-  &.v-enter,
-  &.v-leave-to {
-    opacity: 0;
+    &.v-enter,
+    &.v-leave-to {
+      opacity: 0;
+    }
+  }
+  
+  .bg-white {
+    background-color: $white;
+  }
+  
+  .font-family-serif {
+    font-family: $font-family-serif;
   }
 }
-
 </style>
