271945a9f294c076b8fc94ee1cd2b2123b834ca4
Alex Mendes <alexanderhmendes@gmail.com>
2017-06-16 02:05:25 +0100

    Add missing commas

--- src/pages/account/Register.vue
+++ src/pages/account/Register.vue
@@ -37,7 +37,7 @@ export default {
 
   metaInfo: {
     title: 'Register'
-  }
+  },
 
   components: {
     DefaultLayout,
--- src/pages/account/Signin.vue
+++ src/pages/account/Signin.vue
@@ -73,7 +73,7 @@ export default {
 
   metaInfo: {
     title: 'Sign in'
-  }
+  },
 
   components: {
     DefaultLayout,
