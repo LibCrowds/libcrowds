d94c83c633bcb4277dac75acfc801f373ef49c3a
Alex Mendes <alexanderhmendes@gmail.com>
2017-06-17 04:59:43 +0100

    Update footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,45 +1,35 @@
 <template>
   <footer id="app-footer" class="footer invert-navbar">
-    <div class="container">
+    <div class="container py-3">
 
       <section>
-        <router-link :to="{ path: '/' }">
-          <p class="brand-link">
-            Part of the LibCrowds crowdsourcing platform
-          </p>
-        </router-link>
-        <p v-html="copyright"></p>
-        <ul class="list-inline">
+        <a
+          :href="platform.url"
+          class="brand-link my-2"
+          v-if="config.platform">
+          Part of the {{ config.platform.name }} crowdsourcing platform
+        </a>
+        <p v-html="copyright" class="my-2"></p>
+        <ul class="list-inline my-2">
           <li class="list-inline-item m-0">
-            <a :href="twitterUrl">
+            <a :href="twitterUrl" class="text-muted">
               <icon name="twitter"></icon>
             </a>
           </li>
           <li class="list-inline-item mx-3">
-            <a :href="mailto">
+            <a :href="mailto" class="text-muted">
               <icon name="envelope"></icon>
            </a>
           </li>
           <li class="list-inline-item m-0">
-            <a :href="config.githubUrl">
+            <a :href="config.githubUrl" class="text-muted">
               <icon name="github"></icon>
            </a>
           </li>
         </ul>
       </section>
 
-      <section class="hidden-md-down">
-        <h5 class="list-title">Sites</h5>
-        <ul class="list-unstyled">
-          <li class="list-item" v-for="site in config.sites">
-            <router-link :to="{ name: 'landing' }">
-              {{ site.name }}
-            </router-link>
-          </li>
-        </ul>
-      </section>
-
-      <section class="hidden-md-down">
+      <section class="hidden-md-down my-2">
         <h5 class="list-title">Help</h5>
         <ul class="list-unstyled">
           <li class="list-item">
@@ -92,12 +82,11 @@ export default {
 @import '~bootstrap/scss/bootstrap';
 
 #app-footer.footer {
-  padding-top: 2rem;
-  padding-bottom: 2rem;
   background-color: darken($gray-dark, 5%);
   letter-spacing: 0.8px;
   color: $gray-light;
   font-size: $font-size-sm;
+  line-height: 2;
 
   .container {
     display: flex;
@@ -112,12 +101,9 @@ export default {
   }
 
   .list-title {
+    margin-bottom: 1em;
     text-transform: uppercase;
     font-size: $font-size-sm;
-  }
-
-  ul {
-    margin: 0;
   }
 
   a {
