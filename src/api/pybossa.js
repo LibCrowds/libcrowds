e2b1cb3d08c98dd265496943044e696b32c1a474
Alex Mendes <alex.mendes@bl.uk>
2017-06-19 11:57:34 +0100

    Add missing comma

--- src/api/pybossa.js
+++ src/api/pybossa.js
@@ -8,7 +8,7 @@ const instance = axios.create({
   headers: {
     'Content-Type': 'application/json'
   },
-  timeout: 10000
+  timeout: 10000,
   data: {}  // Must always be set otherwise Content-Type gets deleted
 })
 
