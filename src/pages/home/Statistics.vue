d8d6e3e218cd28e3858378ad957d60797e6d0805
Alex Mendes <alex.mendes@bl.uk>
2017-06-23 10:39:57 +0100

    Fix display of map when no data

--- src/pages/home/Statistics.vue
+++ src/pages/home/Statistics.vue
@@ -11,9 +11,7 @@
         <div id="top-users-chart" class="ct-major-seventh"></div>
       </div>
 
-      <div id="locs-map" style="height: 200px"></div>
-
-      <div v-if="locs.length">
+      <div :style="mapStyle">
         <hr>
         <h3 class="text-center">Locations of Anonymous Volunteers</h3>
         <div class="ct-major-seventh">
@@ -93,6 +91,14 @@ export default {
         return `${baseUrl}/{id}/{z}/{x}/{y}.png?access_token=${token}`
       }
       return 'http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    }
+  },
+
+  computed: {
+    mapStyle: function () {
+      return {
+        display: this.locs.length ? 'block' : 'none'
+      }
     }
   },
 
