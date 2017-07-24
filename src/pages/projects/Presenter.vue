0e5b03716131c2094a7e12b51ab256314e1900e4
Alex Mendes <alex.mendes@bl.uk>
2017-07-24 17:50:55 +0100

    Update presenter styles

--- src/pages/projects/Presenter.vue
+++ src/pages/projects/Presenter.vue
@@ -124,5 +124,9 @@ export default {
   position: absolute;
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
