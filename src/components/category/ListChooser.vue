48834d43145b8b8604c389b333cdc323d263ef86
Alex Mendes <alex.mendes@bl.uk>
2017-07-26 15:12:34 +0100

    Sort categories by name

--- src/components/category/ListChooser.vue
+++ src/components/category/ListChooser.vue
@@ -44,6 +44,7 @@
 </template>
 
 <script>
+import { sortBy } from 'lodash'
 import marked from 'marked'
 import pybossaApi from '@/api/pybossa'
 import Loading from '@/components/Loading'
@@ -76,7 +77,7 @@ export default {
      */
     loadCategories () {
       pybossaApi.get('/').then(r => {
-        this.categories = r.data.categories
+        this.categories = sortBy(r.data.categories, 'name')
         this.changeCategory(this.categories[0])
         this.loading = false
       })
