969e08c046200e67b0c1a9bb5568420024904cb8
Alex Mendes <alexanderhmendes@gmail.com>
2017-08-09 01:54:24 +0100

    Update presenter

--- src/pages/projects/Presenter.vue
+++ src/pages/projects/Presenter.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="presenter">
 
-    <loading
-      v-if="loading"
-      text="Loading project">
-    </loading>
-
     <libcrowds-viewer
-      v-else
       :confirm-before-unload="true"
       :disable-complete="true"
       :show-like="currentUser !== null && currentUser !== undefined"
@@ -24,20 +18,18 @@
 <script>
 import config from '@/config'
 import pybossaApi from '@/api/pybossa'
-import Loading from '@/components/Loading'
 
 export default {
   data: function () {
     return {
-      loading: true,
       project: null,
       currentUser: this.$store.state.currentUser,
       tasks: []
     }
   },
 
-  components: {
-    Loading
+  metaInfo: {
+    title: 'Task Presenter'
   },
 
   computed: {
@@ -73,84 +65,42 @@ export default {
     }
   },
 
-  props: {
-    shortname: String,
-    required: true
-  },
-
   methods: {
     /**
-     * Fetch project with the given short name.
+     * Set core data.
+     * @param {Object} data
+     *   The data.
      */
-    fetchProjects () {
-      console.log(`/api/project?short_name=${this.shortname}`)
-      return pybossaApi.get(`/api/project?short_name=${this.shortname}`)
-    },
-
-    /**
-     * Fetch new tasks.
-     */
-    fetchNewTasks (limit = 100) {
-      const url = `/api/project/${this.project.id}/newtask?limit=${limit}`
-      return pybossaApi.get(url)
-    },
-
-    /**
-     * Fetch a task.
-     */
-    fetchTask (id) {
-      return pybossaApi.get(`/api/task/${id}`)
-    },
-
-    /**
-     * Save the task run.
-     */
-    saveTaskRun (data) {
-      return pybossaApi.post(`/api/taskrun`, data)
-    },
-
-    /**
-     * Add a task to the user's favourites.
-     */
-    addFavourite (taskId) {
-      return pybossaApi.post(`/api/favorites`, {
-        task_id: taskId
-      })
-    },
-
-    /**
-     * Delete a task from the user's favourites.
-     */
-    deleteFavourite (taskId) {
-      return pybossaApi.delete(`/api/favorites/${taskId}`)
+    setData (data) {
+      this.project = data.project
+      this.tasks = data.tasks
     },
 
     /**
      * Handle the task liked event.
+     * @param {Object} task
+     *   The task.
      */
-    onTaskLiked (data) {
-      if (data.liked) {
-        this.addFavourite(data.id)
+    onTaskLiked (task) {
+      if (task.liked) {
+        pybossaApi.post(`/api/favorites`, { task_id: task.id })
       } else {
-        this.deleteFavourite(data.id)
+        pybossaApi.delete(`/api/favorites/${task.id}`)
       }
     },
 
     /**
      * Handle the submit event.
+     * @param {Object} task
+     *   The task.
      */
-    onSubmit (data) {
-      this.fetchTask(data.id).then(r => {
-        let task = r.data
-        task.answer = data.annotations
-        let taskrun = {
-          'project_id': this.project.id,
-          'task_id': data.id,
-          'info': data.annotations
-        }
-        taskrun = JSON.stringify(taskrun)
-        return this.saveTaskRun(taskrun)
-      }).then(r => {
+    onSubmit (task) {
+      const taskrun = JSON.stringify({
+        'project_id': this.project.id,
+        'task_id': task.id,
+        'info': task.annotations
+      })
+      pybossaApi.post(`/api/taskrun`, taskrun).then(r => {
         this.$store.dispatch('NOTIFY', {
           msg: 'Answer saved!',
           type: 'success'
@@ -159,22 +109,28 @@ export default {
     }
   },
 
-  metaInfo: {
-    title: 'Task Presenter'
+  beforeRouteEnter (to, from, next) {
+    let data = {}
+    pybossaApi.get(`/api/project?short_name=${to.params.shortname}`).then(r => {
+      data.project = r.data[0]
+      return pybossaApi.get(`/api/project/${data.project.id}/newtask?limit=100`)
+    }).then(r => {
+      data.tasks = Array.isArray(r.data) ? r.data : [r.data]
+      next(vm => vm.setData(data))
+    })
   },
 
-  mounted () {
-    let limit = 100
-    if ('limit' in this.$store.state.route.query) {
-      limit = this.$store.state.route.query.limit
-    }
-
-    this.fetchProjects().then(r => {
-      this.project = r.data[0]
-      return this.fetchNewTasks(limit)
+  beforeRouteUpdate (to, from, next) {
+    this.project = null
+    this.tasks = []
+    let data = {}
+    pybossaApi.get(`/api/project?short_name=${to.params.shortname}`).then(r => {
+      data.project = r.data[0]
+      return pybossaApi.get(`/api/project/${data.project.id}/newtask?limit=100`)
     }).then(r => {
-      this.tasks = Array.isArray(r.data) ? r.data : [r.data]
-      this.loading = false
+      data.tasks = Array.isArray(r.data) ? r.data : [r.data]
+      this.setData(data)
+      next()
     })
   }
 }
