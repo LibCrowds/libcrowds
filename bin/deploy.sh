8a42bfba5b0abe443e9fe64f62da0dbd9607cdbb
Alex Mendes <alexanderhmendes@gmail.com>
2017-08-13 02:39:09 +0100

    Update deployment script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
+# List of sites in the form ["domain"]="settings"
 SITES=(
   ["playbills.libcrowds.com"]="https://github.com/LibCrowds/site-settings-playbills"
 )
 
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  for i in $( ls ); do
-    echo "Deploying to playbills.libcrowds.com"
+  for site in "${!SITES[@]}"; do
+    echo "Deploying to $site"
 
     # Clear git info
     rm -rf .git
 
     # Configure site
-    git clone https://github.com/LibCrowds/site-settings-playbills src/custom/settings
+    git clone ${SITES[$site]} src/custom/settings
     echo "module.exports = require('@/custom/settings/settings/config.js')" >> src/custom/config.js
 
     # Build
@@ -20,14 +21,14 @@ if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
 
     # Set up package for sending
     git init
-    git remote add deploy "deploy@playbills.libcrowds.com:/var/www/deployment"
+    git remote add deploy "deploy@$site:/var/www/deployment"
     git config user.name "Alex Mendes"
     git config user.email "alexanderhmendes@gmail.com"
     git add --all .
     git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
 
     # Set up permissions
-    echo -e "Host playbills.libcrowds.com\n\tStrictHostKeyChecking no" >> ~/.ssh/config
+    echo -e "Host $site\n\tStrictHostKeyChecking no" >> ~/.ssh/config
     openssl aes-256-cbc -K $encrypted_1cd83addbd20_key -iv $encrypted_1cd83addbd20_iv -in deploy-key.enc -out deploy-key -d
     eval "$(ssh-agent -s)"
     chmod 600 deploy_key
