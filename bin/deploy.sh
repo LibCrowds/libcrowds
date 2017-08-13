921cd8426311cc11f425bbf3b5b3a77556454640
Alex Mendes <alexanderhmendes@gmail.com>
2017-08-13 02:17:58 +0100

    Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
-  echo "Clear git info"
+  echo "Clearing git info"
   rm -rf .git
 
-  echo "Configure playbills site"
+  echo "Configuring playbills site"
   git clone https://github.com/LibCrowds/site-settings-playbills src/custom
   echo "module.exports = require('@/custom/site-settings-playbills/settings/config.js')" >> src/custom/config.js
 
-  echo "Build"
+  echo "Building"
   npm run build
 
-  echo "Set up package for sending"
+  echo "Setting up package for sending"
   git init
   git remote add deploy "deploy@playbills.libcrowds.com:/var/www/deployment"
   git config user.name "Alex Mendes"
@@ -18,13 +18,15 @@ if [ "${TRAVIS_PULL_REQUEST}" == "false" ]; then
   git add --all .
   git commit -m "Deploy from Travis - build {$TRAVIS_BUILD_NUMBER}"
 
-  echo "Set up permissions"
+  echo "Setting up permissions"
   echo -e "Host playbills.libcrowds.com\n\tStrictHostKeyChecking no" >> ~/.ssh/config
   openssl aes-256-cbc -K $encrypted_1cd83addbd20_key -iv $encrypted_1cd83addbd20_iv -in deploy-key.enc -out deploy-key -d
   eval "$(ssh-agent -s)"
   chmod 600 deploy_key
   ssh-add deploy_key
 
-  echo "Deploy"
+  echo "Deploying"
   git push -f deploy
+else
+  echo "Not deploying as this is not the master branch"
 fi
