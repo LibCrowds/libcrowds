7dec016fc11048ff35a626f73197d7e0d2516d74
Alex Mendes <alexanderhmendes@gmail.com>
2017-08-20 23:28:06 +0100

    Use AppBase for help routes

--- src/router/routes/help.js
+++ src/router/routes/help.js
@@ -1,3 +1,4 @@
+import AppBase from '@/pages/AppBase'
 import TOS from '@/pages/help/TOS'
 import Privacy from '@/pages/help/Privacy'
 import Cookies from '@/pages/help/Cookies'
@@ -5,23 +6,35 @@ import Api from '@/pages/help/Api'
 
 export default [
   {
-    path: '/help/tos',
-    name: 'tos',
-    component: TOS
-  },
-  {
-    path: '/help/privacy',
-    name: 'privacy',
-    component: Privacy
-  },
-  {
-    path: '/help/cookies',
-    name: 'cookies',
-    component: Cookies
-  },
-  {
-    path: '/help/api',
-    name: 'api',
-    component: Api
+    path: '/help',
+    component: AppBase,
+    beforeEnter: function (to, from, next) {
+      if (to.fullPath === '/help') {
+        next({ name: '404' })
+      }
+      next()
+    },
+    children: [
+      {
+        path: 'tos',
+        name: 'tos',
+        component: TOS
+      },
+      {
+        path: 'privacy',
+        name: 'privacy',
+        component: Privacy
+      },
+      {
+        path: 'cookies',
+        name: 'cookies',
+        component: Cookies
+      },
+      {
+        path: 'api',
+        name: 'api',
+        component: Api
+      }
+    ]
   }
 ]
