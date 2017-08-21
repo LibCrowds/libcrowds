b809faea8cc4d402158256540fa3af6ad7261cd9
Alex Mendes <alexanderhmendes@gmail.com>
2017-08-21 02:02:02 +0100

    Update config script

--- bin/configure.sh
+++ bin/configure.sh
@@ -1,24 +1,10 @@
 #!/usr/bin/env bash
 
-# Core site settings
-SITESETTINGS="https://github.com/LibCrowds/site-settings-core"
-
-# List of collections configurations in the form "name:settings"
-COLLECTIONSSETTINGS=(
-  "playbills:https://github.com/LibCrowds/site-settings-playbills"
-  "convertacard:https://github.com/LibCrowds/site-settings-convertacard"
-)
+SETTINGS="https://github.com/LibCrowds/libcrowds-frontend-settings"
 
 # Configure site
-rm src/settings/siteConfig.js
-git clone $SITESETTINGS src/settings/tmp
-mv src/settings/tmp/config.js src/settings/siteConfig.js
-rm -rf src/settings/tmp
-
-# Configure projects
-rm -rf src/settings/collections/*/
-for project in "${COLLECTIONSSETTINGS[@]}"; do
-  NAME="${project%%:*}"
-  SETTINGS="${project/$NAME:/}"
-  git clone $SETTINGS src/settings/collections/$NAME
-done
+rm siteConfig.js
+rm -r custom
+mkdir custom
+git clone $SETTINGS src/custom/settings
+echo "module.exports = require('@/custom/settings/siteConfig.js')" >> siteConfig.js
