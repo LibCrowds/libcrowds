ad18e8538dc46525c193a0eb2f5a3534f2907b2b
Alexander Mendes <alexanderhmendes@gmail.com>
2017-09-12 18:01:32 +0100

    Use collection terminology for card stat

--- src/components/project/Card.vue
+++ src/components/project/Card.vue
@@ -42,7 +42,7 @@
         </span>
         <span class="card-stat text-muted mb-2 mb-lg-0">
           <icon name="tasks"></icon> {{ project.n_tasks }}
-          {{ 'task' | pluralize(project.n_tasks) }}
+          {{ collectionConfig.terminology.task | pluralize(project.n_tasks) }}
         </span>
         <span class="card-stat text-muted mb-1 mb-lg-0">
           <icon name="users"></icon> {{ project.n_volunteers }}
