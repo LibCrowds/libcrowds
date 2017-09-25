2985aa6623262d50b83f2296d94c5bfd0c94f278
Alex Mendes <alexanderhmendes@gmail.com>
2017-09-26 00:52:19 +0100

    Remove old util func

--- src/utils/get-scroll-top.js.js
+++
@@ -1,9 +0,0 @@
-/**
- * Cross-browser method to get scroll top (probably needs work).
- */
-function getScrollTop () {
-  const ieScrollTop = document.documentElement.scrollTop
-  return document.body.scrollTop === 0 ? ieScrollTop : document.body.scrollTop
-}
-
-export default getScrollTop
