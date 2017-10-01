92523197cb49bd8b8a7741b0fb501e00852a4bde
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-01 22:57:43 +0100

    Add oauth buttons component

---
+++ src/components/buttons/Oauth.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="oauth-buttons">
+
+    <div v-if="facebook">
+      <b-btn
+        class="mx-1"
+        variant="facebook"
+        @click="redirect('facebook')">
+        <icon name="facebook"></icon>
+        <span class="ml-1 d-none d-sm-block">Facebook</span>
+      </b-btn>
+    </div>
+
+    <div v-if="google">
+      <b-btn
+        class="mx-1"
+        variant="googleplus"
+        @click="redirect('google')">
+        <icon name="google"></icon>
+        <span class="ml-1 d-none d-sm-block">Google</span>
+      </b-btn>
+    </div>
+
+    <div v-if="twitter">
+      <b-btn
+        class="mx-1"
+        variant="twitter"
+        @click="redirect('twitter')">
+        <icon name="twitter"></icon>
+        <span class="ml-1 d-none d-sm-block">Twitter</span>
+      </b-btn>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    facebook: {
+      type: Boolean,
+      default: false
+    },
+    google: {
+      type: Boolean,
+      default: false
+    },
+    twitter: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  methods: {
+    /**
+     * Redirect to an external endpoint for OAuth signin.
+     * @param {String} endpoint
+     *   The endpoint.
+     */
+    redirect (endpoint) {
+      const nextExternal = `${document.location.origin}/${this.next}`
+      const query = `next=${nextExternal}`
+      window.location = `${siteConfig.pybossaHost}/${endpoint}?${query}`
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import 'src/assets/style/main';
+
+#oauth-buttons {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  .btn {
+    @include hover-focus;
+    border-radius: 0;
+    color: $white;
+    height: 100%;
+    display: flex;
+
+    @include hover-focus {
+      color: $white;
+    }
+
+    &.btn-twitter {
+      background: $twitter;
+
+      @include hover-focus {
+        background: darken($twitter, 5%);
+      }
+    }
+
+    &.btn-facebook {
+      background: $facebook;
+
+      @include hover-focus {
+        background: darken($facebook, 5%);
+      }
+    }
+
+    &.btn-googleplus {
+      background: $googleplus;
+
+      @include hover-focus {
+        background: darken($googleplus, 5%);
+      }
+    }
+  }
+
+  /* Fix inconsistend heights */
+  svg {
+    display: flex;
+    height: 1.2rem;
+  }
+}
+</style>
--- src/pages/account/Signin.vue
+++ src/pages/account/Signin.vue
@@ -15,39 +15,11 @@
             <p class="lead text-center">
               or sign in with
             </p>
-            <div class="row-btn-social">
-
-              <div v-if="auth.facebook">
-                <b-btn
-                  class="mx-1"
-                  variant="facebook"
-                  @click="redirect('facebook')">
-                  <icon name="facebook"></icon>
-                  <span class="ml-1 d-none d-sm-block">Facebook</span>
-                </b-btn>
-              </div>
-
-              <div v-if="auth.google">
-                <b-btn
-                  class="mx-1"
-                  variant="googleplus"
-                  @click="redirect('google')">
-                  <icon name="google"></icon>
-                  <span class="ml-1 d-none d-sm-block">Google</span>
-                </b-btn>
-              </div>
-
-              <div v-if="auth.twitter">
-                <b-btn
-                  class="mx-1"
-                  variant="twitter"
-                  @click="redirect('twitter')">
-                  <icon name="twitter"></icon>
-                  <span class="ml-1 d-none d-sm-block">Twitter</span>
-                </b-btn>
-              </div>
-
-            </div>
+            <oauth-buttons
+              :facebook="auth.facebook"
+              :google="auth.google"
+              :twitter="auth.twitter">
+            </oauth-buttons>
           </div>
 
           <div slot="footer-left">
@@ -72,6 +44,7 @@ import 'vue-awesome/icons/facebook'
 import siteConfig from '@/siteConfig'
 import pybossaApi from '@/api/pybossa'
 import CardForm from '@/components/forms/CardForm'
+import OauthButtons from '@/components/buttons/Oauth'
 
 export default {
   data: function () {
@@ -115,7 +88,8 @@ export default {
   },
 
   components: {
-    CardForm
+    CardForm,
+    OauthButtons
   },
 
   computed: {
@@ -125,16 +99,6 @@ export default {
   },
 
   methods: {
-    /**
-     * Redirect to an external endpoint (for OAuth signin).
-     * @param {String} endpoint
-     *   The endpoint
-     */
-    redirect (endpoint) {
-      const nextExternal = `${document.location.origin}/${this.next}`
-      const query = `next=${nextExternal}`
-      window.location = `${siteConfig.pybossaHost}/${endpoint}?${query}`
-    },
 
     /**
      * Set core data.
@@ -167,57 +131,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss" scoped>
-@import 'src/assets/style/main';
-
-#signin {
-  .row-btn-social {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-
-    .btn {
-      @include hover-focus;
-      border-radius: 0;
-      color: $white;
-      height: 100%;
-      display: flex;
-
-      @include hover-focus {
-        color: $white;
-      }
-
-      &.btn-twitter {
-        background: $twitter;
-
-        @include hover-focus {
-          background: darken($twitter, 5%);
-        }
-      }
-
-      &.btn-facebook {
-        background: $facebook;
-
-        @include hover-focus {
-          background: darken($facebook, 5%);
-        }
-      }
-
-      &.btn-googleplus {
-        background: $googleplus;
-
-        @include hover-focus {
-          background: darken($googleplus, 5%);
-        }
-      }
-    }
-
-    /* Fix inconsistend heights */
-    svg {
-      display: flex;
-      height: 1.2rem;
-    }
-  }
-}
-</style>
