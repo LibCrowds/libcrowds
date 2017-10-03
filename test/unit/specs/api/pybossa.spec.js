c48fe117158139565aefba0602189334b0906bc5
Alexander Mendes <alexanderhmendes@gmail.com>
2017-10-03 18:01:41 +0100

    Update tests

--- test/unit/specs/api/pybossa.spec.js
+++ test/unit/specs/api/pybossa.spec.js
@@ -1,22 +1,28 @@
-import axios from 'axios'
-import AxiosMockAdapter from 'axios-mock-adapter'
 import pybossaTestResponses from '../../fixtures/pybossaTestResponses'
 
 import PyBossaApi from '@/api/pybossa'
 
 describe('PyBossaApi', () => {
   let pybossa = null
-  let mockAxios = null
 
   beforeEach(() => {
     pybossa = new PyBossaApi()
-    mockAxios = new AxiosMockAdapter(axios)
+  })
+
+  describe('_filterMicrositeCategories', () => {
+    it('filters microsite categories for the given key only', () => {
+      const categories = pybossaTestResponses.getApiCategories.data
+      const key = 'playbills'
+      const filtered = pybossa._filterMicrositeCategories(categories, key)
+      expect(filtered.length).toBe(1)
+      expect(filtered[0].info.collection).toBe(key)
+    })
   })
 
   describe('getMicrositeCategories', () => {
-    it('builds the URL correctly', () => {
+    it('makes the correct request', () => {
       const mockGet = jest.fn()
-      pybossa.get = mockGet
+      pybossa.client.get = mockGet
       const key = 'playbills'
       const expectedUrl = `/api/${key}`
       const expectedParams = {
@@ -26,6 +32,20 @@ describe('PyBossaApi', () => {
       pybossa.getMicrositeCategories(key)
       expect(mockGet.mock.calls[0][0]).toBe(expectedUrl)
       expect(mockGet.mock.calls[0][1]).toEqual(expectedParams)
+    })
+
+    it('returns the correct categories', () => {
+      const key = 'playbills'
+      const mockGet = jest.fn()
+      const response = pybossaTestResponses.getApiCategories.data
+      pybossa.client.get = mockGet
+      mockGet.mockReturnValue(new Promise((resolve, reject) => {
+        resolve(response)
+      }))
+      return pybossa.getMicrositeCategories(key).then(categories => {
+        expect(categories.length).toBe(1)
+        expect(categories[0].info.collection).toBe(key)
+      })
     })
   })
 })
