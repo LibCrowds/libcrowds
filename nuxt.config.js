532b5eca3fdb4d50133028677b95b7d520b2ae20
Alexander Mendes <alexanderhmendes@gmail.com>
2017-10-10 18:38:48 +0100

    Use eslint-friendly-formatter

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,7 +42,10 @@ module.exports = {
           enforce: 'pre',
           test: /\.(js|vue)$/,
           loader: 'eslint-loader',
-          exclude: /(node_modules)/
+          exclude: /(node_modules)/,
+          options: {
+            formatter: require('eslint-friendly-formatter')
+          }
         })
       }
 
