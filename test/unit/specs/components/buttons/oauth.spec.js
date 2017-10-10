7a40f978fd4cd9d13daaf1a979269529771f63b4
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-10 21:34:06 +0100

    Add test

--- test/unit/specs/components/buttons/oauth.spec.js
+++ test/unit/specs/components/buttons/oauth.spec.js
@@ -5,6 +5,7 @@ import 'vue-awesome/icons/facebook'
 import 'vue-awesome/icons/google-plus'
 import 'vue-awesome/icons/twitter'
 
+import testLocalConfig from '~/test/test.local.config'
 import { mount, createLocalVue } from 'vue-test-utils'
 import Oauth from '~/components/buttons/Oauth'
 
@@ -12,19 +13,31 @@ describe('Oauth buttons', () => {
   const localVue = createLocalVue()
   localVue.use(BootstrapVue)
   localVue.component('icon', Icon)
+  const next = '/'
+  const wrapper = mount(Oauth, {
+    localVue,
+    propsData: {
+      facebook: true,
+      google: true,
+      twitter: true,
+      next: next
+    }
+  })
 
   it('renders all buttons', () => {
     const renderer = require('vue-server-renderer').createRenderer()
-    const wrapper = mount(Oauth, {
-      localVue,
-      propsData: {
-        facebook: true,
-        google: true,
-        twitter: true
-      }
-    })
     renderer.renderToString(wrapper.vm, (err, str) => {
       expect(str).toMatchSnapshot()
     })
+  })
+
+  it('redirects to the correct oauth endpoint', () => {
+    const endpoint = 'twitter'
+    const host = testLocalConfig.pybossa.host
+    const expectedUrl = `${host}/${endpoint}?next=${next}`
+    const mockAssign = jest.fn()
+    window.location.assign = mockAssign
+    wrapper.vm.redirect(endpoint)
+    expect(mockAssign).toHaveBeenCalledWith(expectedUrl)
   })
 })
