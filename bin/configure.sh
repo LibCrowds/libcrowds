48f4b9885ee5338c01e706a1f17ca82b99b23ef9
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-15 22:06:31 +0100

    Add custom config script

---
+++ bin/configure.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SETTINGS="https://github.com/LibCrowds/libcrowds-frontend-settings"
+
+# Configure site
+rm local.config.js
+rm -rf custom/*/
+git clone $SETTINGS custom/settings
+echo "module.exports = require('@/custom/settings/siteConfig.js')" >> local.config.js
