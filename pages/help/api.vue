43d37b07fcfecd38239777fcb34c5052afe5983e
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-15 13:26:03 +0100

    Add help pages

---
+++ pages/help/api.vue
@@ -0,0 +1,40 @@
+<template>
+  <b-card header="API">
+    <p>
+      {{ localConfig.brand }} provides a RESTful API that can be used for
+      retreiving information about tasks, task runs, results, etc. The API
+      expects and returns JSON and is available at:
+    </p>
+    <pre class="my-2">
+      <code>
+        {{ localConfig.pybossaHost }}/api
+      </code>
+    </pre>
+    <p>
+      Some requests will need an API key to authenticate and authorize the
+      operation. You can find your API key listed in your profile.
+    </p>
+    <p>
+      Rather than replicating the documentation here please refer to the
+      official
+      <a href="http://docs.pybossa.com/en/latest/api.html">
+        PYBOSSA API documentation
+      </a>
+      for details.
+    </p>
+  </b-card>
+</template>
+
+<script>
+import localConfig from '@/local.config'
+
+export default {
+  layout: 'dashboard',
+
+  data: function () {
+    return {
+      localConfig: localConfig
+    }
+  }
+}
+</script>
