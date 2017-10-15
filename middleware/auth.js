22e98dd91ce47c7021e1b563f52207c74b0103d6
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-15 21:30:47 +0100

    Fix ssr auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,16 @@
+import axios from 'axios'
 import { manageSession } from '@/utils/auth'
 
+/**
+ * Router middleware to manage the user's session.
+ *
+ * Relies on
+ * @param {Object} context
+ *   The nuxt context.
+ */
 export default function ({ isServer, store, req }) {
   if (isServer && req) {
+    axios.defaults.headers.common.cookie = req.headers.cookie
     manageSession(store, req.headers.cookie)
   }
 }
