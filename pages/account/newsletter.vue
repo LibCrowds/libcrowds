0d8870616299c9aea0f063b59d0daa697da727e9
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-15 20:43:01 +0100

    Update newsletter

--- pages/account/newsletter.vue
+++ pages/account/newsletter.vue
@@ -7,14 +7,15 @@
             {{ lead }}
           </p>
           <template slot="footer">
-            <div class="float-right">
+            <div class="text-right">
               <b-btn
-                variant="outline-secondary"
+                variant="outline-dark"
                 @click="subscribe(false)">
                 No thanks
               </b-btn>
               <b-btn
                 variant="success"
+                class="ml-1"
                 @click="subscribe(true)">
                 Sure, sign me up!
               </b-btn>
