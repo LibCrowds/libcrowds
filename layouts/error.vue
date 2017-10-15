6de27423bd41ab4ba99f8ea16f15dc079997c601
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-15 14:35:08 +0100

    Add basic error page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,36 +1,79 @@
 <template>
-  <section class="container">
-    <h1 class="title">
-      {{ error.statusCode }}
-    </h1>
-    <h2 class="info">
-      {{ error.message }}
-    </h2>
-    <nuxt-link class="button" to="/" v-if="error.statusCode === 404">
-      Homepage
-    </nuxt-link>
-  </section>
+  <main id="error-layout">
+    <div class="container text-center">
+      <icon label="Error" class="mb-2">
+        <icon :name="iconName"></icon>
+        <icon name="circle-thin" scale="2"></icon>
+      </icon>
+
+      <h1 class="title">
+        {{ error.statusCode }}
+      </h1>
+      <h2 class="info">
+        {{ error.message }}
+      </h2>
+      <b-btn
+        variant="dark"
+        size="lg"
+        :to="{
+          name: 'index'
+        }">
+        Home
+      </b-btn>
+    </div>
+  </main>
 </template>
+
 <script>
+import 'vue-awesome/icons/circle-thin'
+import 'vue-awesome/icons/lock'
+import 'vue-awesome/icons/exclamation'
+
 export default {
-  props: ['error']
+  props: {
+    error: {
+      type: Object
+    }
+  },
+
+  computed: {
+    iconName () {
+      if (this.error.statusCode === 403 || this.error.statusCode === 401) {
+        return 'lock'
+      }
+      return 'exclamation'
+    }
+  }
 }
 </script>
 
-<style scoped>
-.title
-{
-  margin-top: 15px;
-  font-size: 5em;
-}
-.info
-{
-  font-weight: 300;
-  color: #9aabb1;
-  margin: 0;
-}
-.button
-{
-  margin-top: 50px;
+<style lang="scss" scoped>
+#error-layout {
+  height: 100vh;
+  display: flex;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .bounce-enter-active {
+    animation: bounce-in .8s;
+  }
+  .bounce-leave-active {
+    animation: bounce-out .5s;
+  }
+  @keyframes bounce-in {
+    0% { transform: scale(0) }
+    50% { transform: scale(1.5) }
+    100% { transform: scale(1) }
+  }
+  @keyframes bounce-out {
+    0% { transform: scale(1) }
+    50% { transform: scale(1.5) }
+    100% { transform: scale(0) }
+  }
 }
 </style>
