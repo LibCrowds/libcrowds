6d5021d0278c58ee0a4bda35d10599ff73af5e68
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-17 02:22:19 +0100

    Add template new collection page

---
+++ pages/admin/collections/new.vue
@@ -0,0 +1,77 @@
+<template>
+  <firebase-form
+    header="New Collection"
+    submit-text="Create"
+    :form="form">
+  </firebase-form>
+</template>
+
+<script>
+import FirebaseForm from '@/components/forms/FirebaseForm'
+
+export default {
+  layout: 'dashboard',
+
+  data () {
+    return {
+      form: {
+        model: {
+          'key': null,
+          'name': null,
+          'tagline': null,
+          'description': null,
+          'forumUrl': null,
+          'presenter': null
+        },
+        schema: {
+          fields: [
+            {
+              model: 'key',
+              label: 'Key',
+              type: 'input',
+              inputType: 'text'
+            },
+            {
+              model: 'name',
+              label: 'Name',
+              type: 'input',
+              inputType: 'text'
+            },
+            {
+              model: 'tagline',
+              label: 'Tagline',
+              type: 'input',
+              inputType: 'text'
+            },
+            {
+              model: 'description',
+              label: 'Description',
+              type: 'textArea',
+              rows: 2
+            },
+            {
+              model: '',
+              label: 'Presenter',
+              type: 'select',
+              values: [
+                'libcrowds-viewer',
+                'z3950'
+              ]
+            }
+          ]
+        }
+      }
+    }
+  },
+
+  head () {
+    return {
+      title: `Collections`
+    }
+  },
+
+  components: {
+    FirebaseForm
+  }
+}
+</script>
