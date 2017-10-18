11305601518a2f72cebe6adf00f2382df1900d1e
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-18 03:06:08 +0100

    Run pybossa API on the server

--- plugins/pybossa.js
+++ plugins/pybossa.js
@@ -1,7 +1,10 @@
-import { createAPI } from '@/api/pybossa'
+import pybossa from '@/api/pybossa'
+import handleFlash from '@/api/interceptors/handleFlash'
 
 export default (ctx, inject) => {
-  const pybossa = createAPI(ctx)
+  // handleFlash intercetor can only be made available on the client
+  pybossa.client.interceptors.response.use((res, ctx) => handleFlash)
+
   ctx.$pybossa = pybossa
   inject('pybossa', pybossa)
 }
