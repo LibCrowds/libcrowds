9dc40468181f0bb307c78caec8c4eb54f8248e3f
Alexander Mendes <alexanderhmendes@gmail.com>
2017-10-19 15:45:55 +0100

    Add batch filter

--- plugins/filters.js
+++ plugins/filters.js
@@ -9,3 +9,23 @@ Vue.filter('pluralize', pluralize)
 Vue.filter('intComma', (n) => {
   return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 })
+
+Vue.filter('batch', (items, n, fillWith = null) => {
+  let batchedItems = []
+  let batch = []
+  for (let item of items) {
+    if (batch.length === n) {
+      batchedItems.push(batch)
+      batch = []
+    }
+    batch.push(item)
+  }
+  if (batch.length) {
+    if (fillWith && batch.length < n) {
+      for (let i = 0; i < n - batch.length; i++) {
+        batch.push(fillWith)
+      }
+    }
+  }
+  return batchedItems
+})
