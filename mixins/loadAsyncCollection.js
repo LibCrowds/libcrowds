0cf7a84c0e95e4107cf7877caad897039a8effeb
Alex Mendes <alexanderhmendes@gmail.com>
2017-10-23 08:42:02 +0100

    Fix loadAsyncCollection

--- mixins/loadAsyncCollection.js
+++ mixins/loadAsyncCollection.js
@@ -19,10 +19,6 @@ export const loadAsyncCollection = {
       }
     }
 
-    if (!params.key) {
-      error({ statusCode: 404, message: 'Page not found' })
-    }
-
     return app.$axios.$get('/api/category', {
       params: {
         short_name: params.shortname
@@ -32,11 +28,16 @@ export const loadAsyncCollection = {
         error({ statusCode: 404, message: 'Page not found' })
       }
       data[0].info = merge(defaultInfo, data[0].info) // Merge default info
+      store.dispatch('UPDATE_COLLECTION', data[0])
       return {
         collection: data[0]
       }
     }).catch(err => {
       error({ statusCode: err.statusCode, message: err.message })
     })
+  },
+
+  validate ({ params }) {
+    return params.shortname !== undefined
   }
 }
