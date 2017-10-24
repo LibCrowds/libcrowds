1c5f7a208d5e88109eafc40df011a6394d702a79
Alexander Mendes <alexanderhmendes@gmail.com>
2017-10-24 18:28:24 +0100

    Improve featured projects table

--- pages/admin/featured.vue
+++ pages/admin/featured.vue
@@ -1,124 +1,49 @@
 <template>
   <div id="admin-featured">
     <b-card no-body header="Set Featured Projects">
-      <b-card-body class="p-0">
-        <b-row no-gutters>
-          <b-col lg="4">
-            <categories-list
-              v-if="categories.length"
-              class="nested-left nested-lg"
-              header="Categories"
-              :categories="categories"
-              @change="onCategoryChange">
-            </categories-list>
-          </b-col>
-          <b-col lg="8" class="p-2">
-            <transition
-              name="fade"
-              mode="out-in"
-              appear>
-              <b-table
-                hover
-                striped
-                show-empty
-                v-if="activeCategory"
-                :items="categoryProjects"
-                :fields="tableFields">
-                <template slot="overall_progress" scope="project">
-                  {{ project.item.overall_progress }}%
-                </template>
-                <template slot="action" scope="project">
-                  <b-btn
-                    :variant="project.item.featured ? 'warning' : 'success'"
-                    size="sm"
-                    block
-                    @click="toggleFeatured(project.item)">
-                    {{ getButtonText(project.item.featured) }}
-                  </b-btn>
-                </template>
-              </b-table>
-            </transition>
-          </b-col>
-        </b-row>
-      </b-card-body>
-    </b-card>
 
-    <b-card
-      no-body
-      class="mt-4"
-      header="Current Featured Projects">
-      <b-table
-        hover
-        striped
-        show-empty
-        :items="featuredProjects"
-        :fields="tableFields">
-        <template slot="overall_progress" scope="project">
-          {{ project.item.overall_progress }}%
-        </template>
+      <div slot="header">
+        <h6 class="mb-1">Featured Projects</h6>
+        <p class="text-muted mb-0">
+          <small>
+            Featured projects appear on their collection microsite homepage.
+          </small>
+        </p>
+      </div>
+
+      <projects-table :search-params="searchParams" no-border>
         <template slot="action" scope="project">
           <b-btn
-            variant="warning"
+            :variant="project.item.featured ? 'warning' : 'success'"
             size="sm"
             block
             @click="toggleFeatured(project.item)">
             {{ getButtonText(project.item.featured) }}
           </b-btn>
         </template>
-      </b-table>
+      </projects-table>
+
     </b-card>
   </div>
 </template>
 
 <script>
-import CategoriesList from '@/components/lists/Categories'
+import { notifications } from '@/mixins/notifications'
+import ProjectsTable from '@/components/tables/Projects'
 
 export default {
   layout: 'admin-dashboard',
 
-  data () {
-    return {
-      activeCategory: null,
-      tableFields: {
-        name: {
-          label: 'Name'
-        },
-        n_volunteers: {
-          label: 'Volunteers',
-          class: 'text-center d-none d-xl-table-cell'
-        },
-        overall_progress: {
-          label: 'Progress',
-          class: 'text-center d-none d-xl-table-cell'
-        },
-        action: {
-          label: 'Action',
-          class: 'text-center'
-        }
-      }
-    }
-  },
-
-  async asyncData ({ app, error }) {
-    const endpoint = `/admin/featured`
-    return app.$axios.$get(endpoint).then(data => {
-      return {
-        categories: data.categories,
-        projects: data.projects
-      }
-    }).catch(err => {
-      error({ statusCode: err.statusCode, message: err.message })
-    })
-  },
+  mixins: [ notifications ],
 
   head () {
     return {
-      title: `Featured Projects`
+      title: 'Featured Projects'
     }
   },
 
   components: {
-    CategoriesList
+    ProjectsTable
   },
 
   methods: {
@@ -140,34 +65,23 @@ export default {
       this.$axios.$put(`/api/project/${project.id}`, {
         featured: !project.featured
       }).then(data => {
-        this.project.featured = !project.featured
+        project.featured = !project.featured
+        this.notify({
+          type: 'success',
+          title: 'Success',
+          message: project.featured
+            ? 'Project added to featured'
+            : 'Project removed from featured'
+        })
       }).catch(err => {
         this.$nuxt.error({ statusCode: err.statusCode, message: err.message })
       })
-    },
-
-    /**
-     * Handle category change.
-     * @param {String} category
-     *   The category.
-     */
-    onCategoryChange (category) {
-      this.activeCategory = category
     }
   },
 
   computed: {
-    categoryProjects () {
-      return this.projects[this.activeCategory.short_name]
-    },
-    featuredProjects () {
-      return Object.keys(this.projects).map(shortName => {
-        return this.projects[shortName].filter(project => {
-          return project.featured
-        })
-      }).reduce((a, b) => {
-        return a.concat(b)
-      }, [])
+    searchParams () {
+      return {}
     }
   }
 }
