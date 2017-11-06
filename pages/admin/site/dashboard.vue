af88b889961bb4ffc0d291c361a13ce0ddad8d0f
Alex Mendes <alexanderhmendes@gmail.com>
2017-11-06 22:53:15 +0000

    Begin admin dashboard

--- pages/admin/site/dashboard.vue
+++ pages/admin/site/dashboard.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="admin-dashboard">
-    <b-card no-body>
-      <div slot="header">
-        <h6 class="mb-1">{{ title }}</h6>
-        <p class="text-muted mb-0">
-          <small>
-            Monitor recent activity.
-          </small>
-        </p>
-      </div>
-    </b-card>
+    <line-chart
+      class="mt-3"
+      header="New users"
+      unit="user"
+      :chart-data="newUsers">
+    </line-chart>
+
+    <line-chart
+      class="mt-3"
+      header="Returning Users"
+      unit="user"
+      :chart-data="returningUsers">
+    </line-chart>
   </div>
 </template>
 
 <script>
+import LineChart from '@/components/charts/Line'
+
 export default {
   layout: 'admin-site-dashboard',
 
@@ -25,10 +30,33 @@ export default {
     }
   },
 
+  async asyncData ({ app, error }) {
+    return app.$axios.$get('/admin/dashboard').then(data => {
+      return {
+        activeAnon: data.active_anon_last_week,
+        activeUsers: data.active_users_last_week,
+        returningUsers: data.returning_users_week,
+        newUsers: data.new_users_week,
+        newTaskRuns: data.new_task_runs_week,
+        newTasks: data.new_tasks_week,
+        updateFeed: data.update_feed,
+        draftProjects: data.draft_projects_last_week,
+        publishedProjects: data.published_projects_last_week,
+        updatedProjects: data.update_projects_last_week
+      }
+    }).catch(err => {
+      error(err)
+    })
+  },
+
   head () {
     return {
       title: this.title
     }
+  },
+
+  components: {
+    LineChart
   }
 }
 </script>
