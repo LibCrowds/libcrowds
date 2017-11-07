8ff883719e014704be8a74c64b704a2b2b4b5eb4
Alexander Mendes <alexanderhmendes@gmail.com>
2017-11-07 15:52:04 +0000

    Remove unecessary wrapper

--- components/navbars/Dashboard.vue
+++ components/navbars/Dashboard.vue
@@ -1,37 +1,34 @@
 <template>
-  <div id="dashboard-navbar">
-    <nav :class="`${position}`">
-      <div class="brand-wrapper navbar-light text-center">
-        <nuxt-link
-          class="brand navbar-brand"
-          :to="{
-            name: 'index'
-          }">
-          {{ localConfig.brand }}
-        </nuxt-link>
-      </div>
+  <nav id="dashboard-navbar" :class="`${position}`">
+    <div class="brand-wrapper navbar-light text-center">
+      <nuxt-link
+        class="brand navbar-brand"
+        :to="{
+          name: 'index'
+        }">
+        {{ localConfig.brand }}
+      </nuxt-link>
+    </div>
 
-      <transition-group
-        appear
-        name="dashboard-slide-in"
-        class="nav"
-        tag="ul"
-        :css="false"
-        @before-enter="beforeEnter"
-        @enter="enter"
-        @leave="leave">
-        <b-nav-item
-          v-for="(item, index) in navItems"
-          exact
-          :key="index"
-          :data-index="index"
-          :to="item.link">
-          {{ item.label }}
-        </b-nav-item>
-      </transition-group>
-
-    </nav>
-  </div>
+    <transition-group
+      appear
+      name="dashboard-slide-in"
+      class="nav"
+      tag="ul"
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave">
+      <b-nav-item
+        v-for="(item, index) in navItems"
+        exact
+        :key="index"
+        :data-index="index"
+        :to="item.link">
+        {{ item.label }}
+      </b-nav-item>
+    </transition-group>
+  </nav>
 </template>
 
 <script>
@@ -70,6 +67,9 @@ export default {
     },
 
     enter (el, done) {
+      if (!el.dataset) {
+        return
+      }
       const delay = el.dataset.index * 150
       setTimeout(() => {
         Velocity(
@@ -95,114 +95,112 @@ export default {
 @import '~assets/style/settings';
 
 #dashboard-navbar {
-  nav {
-    overflow: auto;
-    white-space: nowrap;
-    z-index: 1;
-    background:
-      linear-gradient(rgba($gray-900, 0.8),
-      rgba($gray-900, 0.8)),
-      url('~/assets/img/app-background.jpg');
-    background-size: cover;
-    background-position: center center;
+  overflow: auto;
+  white-space: nowrap;
+  z-index: 1;
+  background:
+    linear-gradient(rgba($gray-900, 0.8),
+    rgba($gray-900, 0.8)),
+    url('~/assets/img/app-background.jpg');
+  background-size: cover;
+  background-position: center center;
+  display: none;
+  font-size: $font-size-xs;
+  text-transform: uppercase;
+
+  .brand-wrapper {
     display: none;
-    font-size: $font-size-xs;
-    text-transform: uppercase;
+    height: $app-navbar-height;
+    background: transparent;
+    align-items: center;
+    justify-content: center;
+    border-bottom: 1px solid rgba($gray-300, 0.5);
+    min-height: $app-navbar-height;
+
+    .brand {
+      margin: 0;
+      color: $white;
+
+      @include hover-focus {
+        color: $white;
+      }
+    }
+  }
+
+  .nav-item {
+    border: none;
+    background-color: transparent;
+    padding: 0rem;
+
+    .nav-link {
+      padding: 0.75rem 1.25rem;
+      width: 100%;
+      color: $white;
+      display: block;
+
+      &:hover,
+      &:focus,
+      &.active {
+        text-decoration: none;
+        background-color: rgba($blue, 0.45);
+      }
+    }
+  }
+
+  &.side {
+    max-height: 100%;
+    height: 100%;
+    flex-direction: column;
+    width: $sidebar-width;
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
 
     .brand-wrapper {
-      display: none;
-      height: $app-navbar-height;
-      background: transparent;
-      align-items: center;
-      justify-content: center;
-      border-bottom: 1px solid rgba($gray-300, 0.5);
-      min-height: $app-navbar-height;
-
-      .brand {
-        margin: 0;
-        color: $white;
-
-        @include hover-focus {
-          color: $white;
-        }
-      }
+      display: flex;
     }
 
     .nav-item {
-      border: none;
-      background-color: transparent;
-      padding: 0rem;
+      margin:
+        calc(#{$list-group-item-padding-x} - #{$list-group-item-padding-y}) 0;
+      font-weight: 400;
+      letter-spacing: 0.8px;
 
-      .nav-link {
-        padding: 0.75rem 1.25rem;
-        width: 100%;
-        color: $white;
-        display: block;
-
-        &:hover,
-        &:focus,
-        &.active {
-          text-decoration: none;
-          background-color: rgba($blue, 0.45);
-        }
+      &:first-child {
+        margin-top: 0;
       }
     }
 
-    &.side {
-      max-height: 100%;
-      height: 100%;
-      flex-direction: column;
-      width: $sidebar-width;
-      position: absolute;
-      top: 0;
-      bottom: 0;
-      left: 0;
-
-      .brand-wrapper {
-        display: flex;
-      }
-
-      .nav-item {
-        margin:
-          calc(#{$list-group-item-padding-x} - #{$list-group-item-padding-y}) 0;
-        font-weight: 400;
-        letter-spacing: 0.8px;
-
-        &:first-child {
-          margin-top: 0;
-        }
-      }
-
-      ul {
-        display: block;
-      }
-
-      @include media-breakpoint-up(lg) {
-        display: flex;
-
-        & + .dashboard {
-          z-index: 2;
-          max-height: 100%;
-          float: right;
-          width: calc(100% - #{$sidebar-width});
-        }
-      }
+    ul {
+      display: block;
     }
 
-    &.top {
-      .nav-item {
-        flex-direction: row;
-        margin: calc(#{$list-group-item-padding-x} - #{$list-group-item-padding-y});
-      }
+    @include media-breakpoint-up(lg) {
+      display: flex;
 
-      ul {
-        padding: 0.5rem 1rem;
-        flex-direction: row;
+      & + .dashboard {
+        z-index: 2;
+        max-height: 100%;
+        float: right;
+        width: calc(100% - #{$sidebar-width});
       }
+    }
+  }
 
-      @include media-breakpoint-down(md) {
-        display: flex;
-      }
+  &.top {
+    .nav-item {
+      flex-direction: row;
+      margin: calc(#{$list-group-item-padding-x} - #{$list-group-item-padding-y});
+    }
+
+    ul {
+      padding: 0.5rem 1rem;
+      flex-direction: row;
+    }
+
+    @include media-breakpoint-down(md) {
+      display: flex;
     }
   }
 }
