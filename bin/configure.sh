08726ae5eea3aff947d3feecf31f013bc278ca91
Alex Mendes <alexanderhmendes@gmail.com>
2017-11-10 10:56:55 +0000

    Remove automate deployment scripts (#413)

--- bin/configure.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SETTINGS="https://github.com/LibCrowds/libcrowds-frontend-settings"
-
-# Configure site
-rm local.config.js
-rm -rf custom/*/
-git clone $SETTINGS custom/settings
-echo "module.exports = require('./custom/settings/siteConfig.js')" >> local.config.js
