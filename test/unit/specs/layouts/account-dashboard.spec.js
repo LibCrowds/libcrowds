f6a9bc21bff9afd983ce7fc51de2e62443df3ea9
Alex Mendes <alexanderhmendes@gmail.com>
2017-11-12 00:31:39 +0000

    Test account dashboard

---
+++ test/unit/specs/layouts/account-dashboard.spec.js
@@ -0,0 +1,43 @@
+import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import BootstrapVue from 'bootstrap-vue'
+
+import { mount, createLocalVue } from 'vue-test-utils'
+import AccountDashboard from '@/layouts/account-dashboard'
+import { routes } from '@/test/fixtures/routes'
+
+describe('App footer', () => {
+  let localVue = null
+  let wrapper = null
+  let store = null
+  let router = null
+
+  beforeEach(() => {
+    localVue = createLocalVue()
+    localVue.use(BootstrapVue)
+    localVue.use(Vuex)
+    localVue.use(VueRouter)
+    store = new Vuex.Store({
+      state: {
+        currentUser: {
+          name: 'joebloggs'
+        }
+      }
+    })
+    router = new VueRouter({
+      routes
+    })
+    wrapper = mount(AccountDashboard, {
+      localVue,
+      store,
+      router
+    })
+  })
+
+  it('renders correctly', () => {
+    const renderer = require('vue-server-renderer').createRenderer()
+    renderer.renderToString(wrapper.vm, (err, str) => {
+      expect(str).toMatchSnapshot()
+    })
+  })
+})
