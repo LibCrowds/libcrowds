85f89b82566f5e30dee1428fd1b85562a99151b8
Alexander Mendes <alexanderhmendes@gmail.com>
2017-11-13 18:52:29 +0000

    Handle registration confirmation

--- mixins/notifications.js
+++ mixins/notifications.js
@@ -38,12 +38,12 @@ export const notifications = {
      *   The response data.
      */
     flash (data) {
-      if (data !== undefined && 'flash' in data) {
+      if (process.browser && data !== undefined && 'flash' in data) {
         if (data.status === 'success') {
           this.notifySuccess({ message: data.flash })
         } else if (data.status === 'warning') {
           this.notifyWarn({ message: data.flash })
-        } else if (data.status === 'info') {
+        } else if (data.status === 'info' || data.status === 'message') {
           this.notifyInfo({ message: data.flash })
         } else {
           this.notify({
---
+++ pages/account/register/confirmation.vue
@@ -0,0 +1,28 @@
+<template>
+<p>OK</p>
+</template>
+
+<script>
+import { notifications } from '@/mixins/notifications'
+
+export default {
+  mixins: [ notifications ],
+
+  asyncData ({ app, error, redirect, route }) {
+    return app.$axios.$get('/account/register/confirmation', {
+      params: route.query
+    }).then(data => {
+      return {
+        response: data
+      }
+    }).catch(err => {
+      error(err)
+    })
+  },
+
+  created () {
+    this.flash(this.response)
+    this.$router.push({ path: this.response.next })
+  }
+}
+</script>
