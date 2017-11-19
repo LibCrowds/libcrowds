95e60fa399140904a7e9e32ed15923f45b961846
Alex Mendes <alexanderhmendes@gmail.com>
2017-11-19 02:26:15 +0000

    Update nuxt client init

--- nuxt.config.js
+++ nuxt.config.js
@@ -110,7 +110,7 @@ const config = {
     { src: '~/plugins/filters' },
     { src: '~/plugins/libcrowds-viewer', ssr: false },
     { src: '~/plugins/notifications', ssr: false },
-    { src: '~/plugins/nuxtClientInit', ssr: false },
+    { src: '~/plugins/nuxt-client-init', ssr: false },
     { src: '~/plugins/vue-awesome' },
     { src: '~/plugins/vue-chartist', ssr: false },
     { src: '~/plugins/vue-clickaway', ssr: false },
--- plugins/nuxt-client-init.js
+++ plugins/nuxt-client-init.js
@@ -4,6 +4,8 @@
  * This is used to that we can asynchronously perform actions that are only
  * available on the client, before rendering.
  */
-export default async (ctx) => {
-  await ctx.store.dispatch('nuxtClientInit', ctx)
+export default (ctx) => {
+  if (ctx.isClient) {
+    ctx.store.dispatch('nuxtClientInit', ctx)
+  }
 }
