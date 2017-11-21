ca5dc0967164b2476ce28eb467d0b5975339cb20
Alex Mendes <alexanderhmendes@gmail.com>
2017-11-21 23:10:12 +0000

    Fix help cards

--- pages/help/api.vue
+++ pages/help/api.vue
@@ -1,27 +1,29 @@
 <template>
   <card-base :title="title" :description="description">
-    <p>
-      {{ localConfig.brand }} provides a RESTful API that can be used for
-      retreiving information about tasks, task runs, results, etc. The API
-      expects and returns JSON and is available at:
-    </p>
-    <pre class="mt-2 mb-0">
-      <code>
-        {{ apiBase }}
-      </code>
-    </pre>
-    <p>
-      Some requests will need an API key to authenticate and authorize the
-      operation. You can find your API key listed in your profile.
-    </p>
-    <p>
-      Rather than replicating the documentation here please refer to the
-      official
-      <a target="_blank" href="http://docs.pybossa.com/api/">
-        PYBOSSA API documentation
-      </a>
-      for details.
-    </p>
+    <b-card-body>
+      <p>
+        {{ localConfig.brand }} provides a RESTful API that can be used for
+        retreiving information about tasks, task runs, results, etc. The API
+        expects and returns JSON and is available at:
+      </p>
+      <pre class="mt-2 mb-0">
+        <code>
+          {{ apiBase }}
+        </code>
+      </pre>
+      <p>
+        Some requests will need an API key to authenticate and authorize the
+        operation. You can find your API key listed in your profile.
+      </p>
+      <p>
+        Rather than replicating the documentation here please refer to the
+        official
+        <a target="_blank" href="http://docs.pybossa.com/api/">
+          PYBOSSA API documentation
+        </a>
+        for details.
+      </p>
+    </b-card-body>
   </card-base>
 </template>
 
