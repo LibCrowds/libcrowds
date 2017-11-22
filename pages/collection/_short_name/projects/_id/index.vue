89c5504197ad983fab97cabb14bbbfb2ad1a331a
Alexander Mendes <alexanderhmendes@gmail.com>
2017-11-22 17:02:08 +0000

    Add redirect page for collection projects root

---
+++ pages/collection/_short_name/projects/_id/index.vue
@@ -0,0 +1,31 @@
+<template>
+</template>
+
+<script>
+import { fetchProjectAndCollection } from '@/mixins/fetchProjectAndCollection'
+
+export default {
+  mixins: [ fetchProjectAndCollection ],
+
+  computed: {
+    collection () {
+      return this.$store.state.currentCollection
+    },
+
+    project () {
+      return this.$store.state.currentProject
+    }
+  },
+
+  created () {
+    this.$router.push({
+      name: 'collection-short_name-projects-id-presenter',
+      params: {
+        short_name: this.collection.short_name,
+        id: this.project.id,
+        presenter: this.collection.info.presenter
+      }
+    })
+  }
+}
+</script>
