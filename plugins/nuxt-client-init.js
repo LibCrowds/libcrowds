2be944bb4c05efefc8062f2b5a2f44431b5d0ddc
Alexander Mendes <alexanderhmendes@gmail.com>
2017-11-24 16:56:22 +0000

    Remove unecessary from nuxtClientInit

--- plugins/nuxt-client-init.js
+++ plugins/nuxt-client-init.js
@@ -4,8 +4,6 @@
  * This is used to that we can asynchronously perform actions that are only
  * available on the client, before rendering.
  */
-export default async (ctx) => {
-  if (ctx.isClient) {
-    await ctx.store.dispatch('nuxtClientInit', ctx)
-  }
+export default (ctx) => {
+  ctx.store.dispatch('nuxtClientInit', ctx)
 }
