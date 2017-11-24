78d9b7ea24ae2cb0fc47128ee957505b6119fb37
Alex Mendes <alexanderhmendes@gmail.com>
2017-11-24 03:09:30 +0000

    Update test settings

--- test/test.local.config.js
+++ test/test.local.config.js
@@ -1,49 +1,12 @@
-const config = {
+module.exports = {
   company: 'My Company',
   brand: 'My Brand',
   tagline: 'My inspiring tagline',
   description: 'My SEO optimised meta description',
   pybossaHost: 'http://example.com',
-  libcrowdsHost: 'http://mylibcrowds.com'
+  libcrowdsHost: 'http://mylibcrowds.com',
+  twitter: 'mytwitterhandle',
+  email: 'me@example.com',
+  github: 'https://github.com/Organization/repo',
+  docs: 'http://docs.example.com'
 }
-
-// Twitter
-config.twitter = 'mytwitterhandle'
-
-// Contact email address
-config.email = 'me@example.com'
-
-// GitHub account
-config.github = 'https://github.com/LibCrowds/libcrowds'
-
-config.docs = 'http://docs.example.com'
-
-config.footer = {
-  title: 'Newsletter',
-  items: [
-    { text: 'Sign up', url: 'http://mailchimp.signup.url' }
-  ]
-}
-
-// List of collection configurations
-// config.collections = {
-//   'default': {
-//     name: 'My Amazing Project',
-//     tagline: 'This project is amazing.',
-//     description: 'Help enhance research.'
-//   }
-// }
-
-// Mapbox configuration for location stats
-// config.mapbox = {
-//   id: 'mapbox.streets',
-//   publicApiToken: 'token'
-// }
-
-// Sentry public DSN
-// config.sentryPublicDsn = 'https://******@sentry.io/<project>'
-
-// Google analytics tracking ID
-// config.analytics = 'UA-XXX-X'
-
-module.exports = config
