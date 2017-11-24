d5ae24129d8e3b6d63e11bae9c52a3dbbffc1a42
Alexander Mendes <alexanderhmendes@gmail.com>
2017-11-24 17:06:34 +0000

    Remove nuxt client init

--- plugins/nuxt-client-init.js
+++
@@ -1,9 +0,0 @@
-/**
- * Dispatch the nuxt client init event.
- *
- * This is used to that we can asynchronously perform actions that are only
- * available on the client, before rendering.
- */
-export default (ctx) => {
-  ctx.store.dispatch('nuxtClientInit', ctx)
-}
--- store/index.js
+++ store/index.js
@@ -4,11 +4,8 @@ import mutations from '@/store/mutations'
 import actions from '@/store/actions'
 
 actions.nuxtServerInit = async ({ dispatch }, { app }) => {
-  await dispatch('UPDATE_PUBLISHED_COLLECTIONS', app.$axios)
-}
-
-actions.nuxtClientInit = async ({ dispatch }, { app }) => {
   await dispatch('UPDATE_CURRENT_USER', app.$axios)
+  await dispatch('UPDATE_PUBLISHED_COLLECTIONS', app.$axios)
   await dispatch('UPDATE_LAST_ANNOUNCEMENT', app.$axios)
 }
 
