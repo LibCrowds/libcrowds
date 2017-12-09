483bab049b3a7d434681888830419996fe34bdad
Alex Mendes <alexanderhmendes@gmail.com>
2017-12-09 01:29:31 +0000

    Refactor delete collection page

--- pages/admin/collection/_short_name/delete.vue
+++ pages/admin/collection/_short_name/delete.vue
@@ -1,15 +1,15 @@
 <template>
   <card-base :title="title" :description="description">
-    <b-card-body v-if="collection.nProjects === 0">
+    <b-card-body v-if="canDelete">
       <b-alert show variant="danger">
         <strong>Danger:</strong> Deleting a collection microsite is final,
         there is no undo!
       </b-alert>
     </b-card-body>
-    <b-card-body v-else>
+    <b-card-body v-else-if="!loading">
       <b-alert show variant="warning">
-        <strong>Warning:</strong> You cannot delete collections that contain
-        projects, please move all projects to another collection first.
+        You cannot delete this collection as it contains active projects.
+        Please move all projects to another collection first.
       </b-alert>
     </b-card-body>
     <b-card-footer>
@@ -17,7 +17,7 @@
         <b-btn
           variant="danger"
           class="ml-auto"
-          :disabled="collection.nProjects === 0"
+          :disabled="!canDelete"
           @click="deleteCollection">
           Delete
         </b-btn>
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import { fetchCollectionByName } from '@/mixins/fetchCollectionByName'
 import { deleteDomainObject } from '@/mixins/deleteDomainObject'
 import { metaTags } from '@/mixins/metaTags'
 import CardBase from '@/components/cards/Base'
@@ -34,41 +35,15 @@ import CardBase from '@/components/cards/Base'
 export default {
   layout: 'admin-collection-dashboard',
 
-  mixins: [ deleteDomainObject, metaTags ],
+  mixins: [ fetchCollectionByName, deleteDomainObject, metaTags ],
 
   data () {
     return {
       title: 'Delete',
-      description: 'Permanently delete the microsite.'
+      description: 'Permanently delete the microsite.',
+      canDelete: false,
+      loading: true
     }
-  },
-
-  fetch ({ params, app, error, store }) {
-    // Fetch the category and check if it has any projects
-    let category = null
-    return app.$axios.$get('/api/category', {
-      params: {
-        all: 1,
-        short_name: params.short_name
-      }
-    }).then(categoriesData => {
-      if (!categoriesData || categoriesData.length !== 1) {
-        error({ statusCode: 404 })
-        return
-      }
-      category = categoriesData[0]
-      return app.$axios.$get('/api/project', {
-        params: {
-          all: 1,
-          category_id: category.id
-        }
-      })
-    }).then(projectsData => {
-      category.nProjects = projectsData.length
-      store.dispatch('UPDATE_CURRENT_COLLECTION', category)
-    }).catch(err => {
-      error(err)
-    })
   },
 
   computed: {
@@ -94,6 +69,18 @@ export default {
         this.$router.push({ name: 'admin-collection' })
       })
     }
+  },
+
+  created () {
+    this.$axios.$get('/api/project', {
+      params: {
+        all: 1,
+        category_id: this.collection.id
+      }
+    }).then(data => {
+      this.canDelete = data.length < 1
+      this.loading = false
+    })
   }
 }
 </script>
