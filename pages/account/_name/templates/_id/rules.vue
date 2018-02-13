8f0f679e7a49efe6226ebc18baab766ed7ba9eeb
Alex Mendes <alexanderhmendes@gmail.com>
2018-02-13 14:52:29 +0000

    Add date rules to templates

--- pages/account/_name/templates/_id/rules.vue
+++ pages/account/_name/templates/_id/rules.vue
@@ -13,12 +13,14 @@
       :form="form">
 
       <div slot="bottom" class="d-flex form-group mt-1">
-        <toggle-button
-          :value="form.model.trim_punctuation"
-          :sync="true"
-          :labels="true"
-          @change="updateModelBoolean('trim_punctuation', $event)">
-        </toggle-button>
+        <no-ssr>
+          <toggle-button
+            :value="form.model.trim_punctuation"
+            :sync="true"
+            :labels="true"
+            @change="updateModelBoolean('trim_punctuation', $event)">
+          </toggle-button>
+        </no-ssr>
         <label class="ml-1">
           Trim punctuation from the begining and end of transcribed text
         </label>
@@ -28,12 +30,14 @@
         v-if="presenter == 'iiif-annotation'"
         slot="bottom"
         class="d-flex form-group mt-1">
-        <toggle-button
-          :value="form.model.concatenate"
-          :sync="true"
-          :labels="true"
-          @change="updateModelBoolean('concatenate', $event)">
-        </toggle-button>
+        <no-ssr>
+          <toggle-button
+            :value="form.model.concatenate"
+            :sync="true"
+            :labels="true"
+            @change="updateModelBoolean('concatenate', $event)">
+          </toggle-button>
+        </no-ssr>
         <label class="ml-1">
           Concatenate all fields into a single annotation
         </label>
@@ -43,12 +47,103 @@
         v-if="presenter == 'iiif-annotation'"
         slot="bottom"
         class="d-flex form-group mt-1">
-        <toggle-button
-          :value="form.model.target_from_select_parent"
-          :sync="true"
-          :labels="true"
-          @change="updateModelBoolean('target_from_select_parent', $event)">
-        </toggle-button>
+        <no-ssr>
+          <toggle-button
+            :value="form.model.target_from_select_parent"
+            :sync="true"
+            :labels="true"
+            @change="updateModelBoolean('date_format', $event)">
+          </toggle-button>
+        </no-ssr>
+        <label class="ml-1">
+          Format the transcription as a date
+        </label>
+      </div>
+      <div
+        v-if="presenter == 'iiif-annotation'"
+        slot="bottom"
+        class="form-group mt-1">
+        <div class="d-flex">
+          <no-ssr>
+            <toggle-button
+              :value="form.model.date_format"
+              :sync="true"
+              :labels="true"
+              @change="updateModelBoolean('date_format', $event)">
+            </toggle-button>
+          </no-ssr>
+          <label class="ml-1">
+            Format transcribed dates
+          </label>
+        </div>
+        <div class="hint">
+          Parses most known date formats to improve comparision between
+          inconsistent transcriptions and output the final results according
+          to the ISO 8601 standard (YYYY-MM-DD).
+        </div>
+      </div>
+
+      <div
+        v-if="presenter == 'iiif-annotation' && form.model.date_format"
+        slot="bottom"
+        class="form-group mt-1">
+        <div class="d-flex">
+          <no-ssr>
+            <toggle-button
+              :value="form.model.yearfirst"
+              :sync="true"
+              :labels="true"
+              @change="updateModelBoolean('yearfirst', $event)">
+            </toggle-button>
+          </no-ssr>
+          <label class="ml-1">
+            Interpret the first value in an ambiguous 3-integer date
+            (e.g. 01/05/09) as the year.
+          </label>
+        </div>
+        <div class="hint">
+           If set to True, the first number is taken to be the year, otherwise
+           the last number is taken to be the year.
+        </div>
+      </div>
+
+      <div
+        v-if="presenter == 'iiif-annotation' && form.model.date_format"
+        slot="bottom"
+        class="form-group mt-1">
+        <div class="d-flex">
+          <no-ssr>
+            <toggle-button
+              :value="form.model.dayfirst"
+              :sync="true"
+              :labels="true"
+              @change="updateModelBoolean('dayfirst', $event)">
+            </toggle-button>
+          </no-ssr>
+          <label class="ml-1">
+            Whether to interpret the first value in an ambiguous 3-integer
+            date (e.g. 01/05/09) as the day.
+          </label>
+        </div>
+        <div class="hint">
+           If set to True, the first number is taken to be the day, otherwise
+           the first number is taken to be the month. If yearfirst is set to
+           True, this distinguishes between YDM and YMD.
+        </div>
+      </div>
+
+      <div
+        v-if="presenter == 'iiif-annotation'"
+        slot="bottom"
+        class="d-flex form-group mt-1">
+        <no-ssr>
+          <toggle-button
+            :value="form.model.target_from_select_parent"
+            :sync="true"
+            :labels="true"
+            @change="updateModelBoolean('target_from_select_parent', $event)">
+          </toggle-button>
+        </no-ssr>
         <label class="ml-1">
           Set annotation target from parent selection task
         </label>
