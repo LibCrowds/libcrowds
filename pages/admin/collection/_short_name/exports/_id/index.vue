8a147fc0c5e4d6667de59c536ccb9661e4a87db8
Alexander Mendes <alexanderhmendes@gmail.com>
2018-04-10 18:25:22 +0100

    Use same schema for custom export format forms (see #703)

--- pages/admin/collection/_short_name/exports/_id/index.vue
+++ pages/admin/collection/_short_name/exports/_id/index.vue
@@ -9,47 +9,6 @@
       submit-text="Add Export Format"
       :form="form"
       @success="onSuccess">
-
-      <div slot="bottom" class="form-group">
-        <label>Motivation</label>
-        <multiselect
-          v-model="formModel.motivation"
-          label="name"
-          track-by="id"
-          :options="motivations">
-        </multiselect>
-        <div class="hint">
-          The annotation motivation.
-        </div>
-      </div>
-
-      <div slot="bottom" class="form-group">
-        <label>Root Template</label>
-        <multiselect
-          v-model="formModel.root_template_id"
-          label="name"
-          track-by="id"
-          :options="currentCollection.info.templates">
-        </multiselect>
-        <div class="hint">
-          The root template (leave blank to include all data).
-        </div>
-      </div>
-
-      <div slot="bottom" class="form-group">
-        <label>Include</label>
-        <multiselect
-          v-model="formModel.include"
-          label="name"
-          track-by="id"
-          :options="currentCollection.info.templates"
-          :multiple="true">
-        </multiselect>
-        <div class="hint">
-          Include additional templates not already within the chosen hierarchy.
-        </div>
-      </div>
-
     </pybossa-form>
 
   </card-base>
@@ -58,23 +17,19 @@
 <script>
 import { fetchCollectionByName } from '@/mixins/fetchCollectionByName'
 import { metaTags } from '@/mixins/metaTags'
+import { customExportSchema } from '@/mixins/forms/customExportSchema'
 import CardBase from '@/components/cards/Base'
 import PybossaForm from '@/components/forms/PybossaForm'
 
 export default {
   layout: 'admin-collection-dashboard',
 
-  mixins: [ fetchCollectionByName, metaTags ],
+  mixins: [ fetchCollectionByName, metaTags, customExportSchema ],
 
   data () {
     return {
-      title: 'Add Export Format',
-      description: 'Add an export format.',
-      motivations: [
-        { id: 'describing', name: 'Describing' },
-        { id: 'tagging', name: 'Tagging' },
-        { id: 'commenting', name: 'Commenting' }
-      ]
+      title: 'Update Export Format',
+      description: 'Update an export format.'
     }
   },
 
@@ -105,31 +60,7 @@ export default {
         endpoint: this.endpoint,
         method: 'post',
         model: this.formModel,
-        schema: {
-          fields: [
-            {
-              model: 'name',
-              label: 'Name',
-              type: 'input',
-              inputType: 'text',
-              onChanged: (model, newVal, oldVal, field) => {
-                const newSn = this.getShortname(newVal)
-                const oldSn = this.getShortname(oldVal)
-                if (!model.short_name || model.short_name === oldSn) {
-                  model.short_name = newSn
-                }
-              },
-              hint: 'The name of the custom export.'
-            },
-            {
-              model: 'short_name',
-              label: 'Short Name',
-              type: 'input',
-              inputType: 'text',
-              hint: 'An identifier used in the filenames of downloaded files.'
-            }
-          ]
-        }
+        schema: this.customExportSchema
       }
     }
   },
